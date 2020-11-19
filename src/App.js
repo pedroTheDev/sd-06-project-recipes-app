b4e970fcd8396e439dab4bfaff310b82d3436e33
DehSartoreto <fixsartoreto@gmail.com>
2020-11-19 19:03:43 -0300

    Header finalizado sem SearchBar API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Provider from './context/Provider';
-import Login from './components/Login';
+import Login from './pages/Login';
+import Foods from './pages/Foods';
+import Perfil from './pages/Perfil';
 
 function App() {
   return (
     <Provider>
       <Switch>
         <Route exact path="/" component={Login} />
+        <Route path="/comidas" component={Foods} />
+        <Route path="/perfil" component={Perfil} />
       </Switch>
     </Provider>
   );
