567080afaac46fb8abee4e66f25171185fa9cd85
Paulo <paulocheloni@gmail.com>
2020-11-19 19:25:30 -0300

    adding login page (in progress)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import renderWithRouter from './helpers/renderWithRouter';
 
 test('Farewell, front-end', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/TRYBE/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('test LoginPage', () => {
+  const { queryByTestId, history } = renderWithRouter(<Login />)
+  expect(history.location.pathname).toBe('/');
+  const email = queryByTestId('email-input');
+  const password = queryByTestId('password-input');
+  const loginButton = queryByTestId('login-submit-btn');
+  expect(email).toBeInTheDocument();
+  expect(password).toBeInTheDocument();
+  expect(loginButton).toBeInTheDocument();
+  expect(loginButton.innerText).toBe('Entrar');
 });
