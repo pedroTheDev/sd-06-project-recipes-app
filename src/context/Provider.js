dc049b921d06e89a16bcd2a150db9f1926bb8957
Cássio Rubens <cassiorubensadv@gmail.com>
2020-11-19 19:22:59 -0300

    FEAT: Login screen

---
+++ src/context/Provider.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import RevenueContext from './RevenueContext';
+
+const Provider = ({ children }) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const context = {
+    email, password, setEmail, setPassword,
+  };
+  return (
+    <RevenueContext.Provider value={context}>
+      {children}
+    </RevenueContext.Provider>
+  );
+};
+
+Provider.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default Provider;
---
+++ src/context/RevenueContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const RevenueContext = createContext();
+
+export default RevenueContext;
