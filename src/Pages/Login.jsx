a03b4cc8b694904c83bdf1ef0c8e56c38b610212
Isadora Koga <isadorakoga710@gmail.com>
2020-11-19 16:55:58 -0300

    :memo: Req 2, 3, 4, 5 + tests

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [buttonDisable, setButtonDisable] = useState(false);
+  const [userEmail, setUserEmail] = useState('');
+
+  const checkButton = () => {
+    const inputPassword = document.getElementById('password-input').value;
+    const inputEmail = document.getElementById('email-input').value;
+    const regex = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      setButtonDisable(inputPassword.length > 6 && regex.test(inputEmail));
+      setUserEmail(inputEmail);
+  }
+
+  return (
+    <div>
+      <label htmlFor="email-input">
+        Email:
+        <input
+          type="text" data-testid="email-input"
+          id="email-input"
+          onChange={checkButton}
+        />
+      </label>
+      <label htmlFor="password-input">
+        Password:
+      <input
+        type="password"
+        data-testid="password-input"
+        id="password-input"
+        onChange={checkButton}
+      />
+    </label>
+    <button
+      type="submit"
+      data-testid="login-submit-btn"
+      disabled={!buttonDisable}
+    >
+      Entrar
+    </button>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/Tests/Login.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import Login from '../Pages/Login';
+
+
+describe('Teste página de Login', () => {
+  it('Crie os elementos que devem respeitar os atributos descritos no protótipo', () => {
+    const { getByTestId } = renderWithRouter(<Login />);
+    const emailTest = getByTestId('email-input');
+    const passwordTest = getByTestId('password-input');
+    const buttonLoginTest = getByTestId('login-submit-btn');
+
+    expect(emailTest).toBeInTheDocument();
+    expect(passwordTest).toBeInTheDocument();
+    expect(buttonLoginTest).toBeInTheDocument();
+  });
+
+  it('o formulário só seja válido após um email válido e uma senha de mais de 6 caracteres serem preenchidos', () => {
+    const { getByTestId } = renderWithRouter(<Login />);
+    const button = getByTestId('login-submit-btn');
+    expect(button).toBeDisabled();
+    const email = getByTestId('email-input');
+    const senha = getByTestId('password-input');
+    userEvent.type(email, 'email');
+    userEvent.type(senha, '123456');
+    expect(button).toBeDisabled();
+    userEvent.type(email, 'email@com@');
+    userEvent.type(senha, '123456');
+    expect(button).toBeDisabled();
+    userEvent.type(email, 'emailcom@');
+    userEvent.type(senha, '123456');
+    expect(button).toBeDisabled();
+    userEvent.type(email, 'alguem@email.com');
+    userEvent.type(senha, '1234567');
+    expect(button).toBeEnabled();
+  });
+});
