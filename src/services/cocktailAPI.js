b6fffd4c57e07c4963c813b1ba899e8b168c69e8
isabellavjs <isabellavjs@gmail.com>
2020-11-19 16:52:26 -0300

    Adiciona endpoints

---
+++ src/services/cocktailAPI.js
@@ -0,0 +1,3 @@
+const COCKTAIL_API = 'https://www.thecocktaildb.com/api/json/v1/1';
+
+// Endpoints
---
+++ src/services/mealAPI.js
@@ -0,0 +1,6 @@
+const MEAL_API = 'https://www.themealdb.com/api/json/v1/1/';
+
+// Endpoints
+const filterByIngredientEndpoint = 'filter.php?i=';
+const searchByNameEndpoint = 'search.php?s=';
+const searchByFirstLetterEndpoint = 'search.php?f=';
