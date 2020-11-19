2f7d2217ea0d6236f976a93eb5769b59bd1b6073
Marcelo Pessini <marceloppessini@gmail.com>
2020-11-19 18:22:14 -0300

    Requisito09

---
+++ src/pages/MainDrink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderDrink from '../components/HeaderDrink';
+
+function MainFood() {
+  return (
+    <div>
+      <HeaderDrink />
+    </div>
+  );
+}
+
+export default MainFood;
---
+++ src/pages/MainFood.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderFood from '../components/HeaderFood';
+
+function MainFood() {
+  return (
+    <div>
+      <HeaderFood />
+    </div>
+  );
+}
+
+export default MainFood;
