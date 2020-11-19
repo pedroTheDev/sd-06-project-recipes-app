48a319bde474e60b2353dc4151a172d3acc04800
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-11-19 16:58:27 -0300

    Requisitos 2 ao 6

---
+++ src/pages/Login.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [disabled, setDisabled] = useState(true);
+  const [user, setUser] = useState({
+    email: '',
+    password: 0,
+  });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    setUser({
+      ...user,
+      [name]: value
+    });
+
+    inputValues();
+  };
+
+  const inputValues = () => {
+    const { email, password } = user;
+    const regexp = /^[a-zA-Z0-9.!#$%&_-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    let disabled = true;
+    const six = 6;
+
+    if (regexp.test(email) && password.length >= six) disabled = false;
+
+    setDisabled(disabled);
+  };
+
+  const handleClick = () => {
+
+    setUserOnLocalStorage();
+  };
+
+  const setUserOnLocalStorage = () => {
+    const { email } = user;
+
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+  }
+
+  return (
+    <section>
+      <form>
+        <label htmlfor="email-input">Email</label>
+        <input id="email-input" name="email" type="text" data-testid="email-input" onChange={ handleChange } />
+        <label htmlfor="password-input">Senha</label>
+        <input id="password-input" name="password" type="password" data-testid="password-input" onChange={ handleChange } />
+        <button
+          disabled={disabled}
+          type="button"
+          data-testid="login-submit-btn"
+          onClick={ handleClick }
+        >
+          Login
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default Login;
