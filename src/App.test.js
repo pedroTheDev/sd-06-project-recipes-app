9be9374564c77176083e71541a1585b6964c7807
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-11-19 20:59:00 -0300

    Requisito 5 - Implementado e testado

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, fireEvent, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './tests/renderWithRouter';
 import App from './App';
 
@@ -21,5 +22,54 @@ describe('Tela de Login - todos os elementos devem respeitar os atributos', () =
     expect(email).toBeInTheDocument('');
     expect(password).toBeInTheDocument('');
     expect(button).toBeInTheDocument('');
+  });
+});
+
+describe('Tela de Login - Formulario só será valido se ambos email e senha sejam validos', () => {
+  beforeEach(cleanup);
+
+  it('O botão deve estar desativado se o email for invalido', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const email = getByTestId('email-input');
+    const password = getByTestId('password-input');
+    const button = getByTestId('login-submit-btn');
+
+    userEvent.type(email, 'email@com@');
+    userEvent.type(password, 'dhs3u8');
+    expect(button).toBeDisabled();
+
+    userEvent.type(email, 'alguem@email.com');
+    userEvent.type(password, 'jdu46c');
+    expect(button).toBeEnabled();
+  });
+
+  it('O botão deve estar desativado se a senha não tiver pelo menos 6 caracteres', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const email = getByTestId('email-input');
+    const password = getByTestId('password-input');
+    const button = getByTestId('login-submit-btn');
+
+    userEvent.type(email, 'alguem@email.com');
+    userEvent.type(password, 'dj3');
+    expect(button).toBeDisabled();
+
+    userEvent.type(email, 'alguem@email.com');
+    userEvent.type(password, 'jdu46c');
+    expect(button).toBeEnabled();
+  });
+
+  it('O botão deve estar desativado se o email for invalido', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const email = getByTestId('email-input');
+    const password = getByTestId('password-input');
+    const button = getByTestId('login-submit-btn');
+    
+    userEvent.type(email, 'jaksjdl');
+    userEvent.type(password, '1234');
+    expect(button).toBeDisabled();
+
+    userEvent.type(email, 'alguem@email.com');
+    userEvent.type(password, 'jdu46c');
+    expect(button).toBeEnabled();
   });
 })
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,24 +3,36 @@ import React, { useState } from 'react';
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  // const isValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  // const isValid = /^.{6,}$/.test(password); 
-  const handleChange = ({target}) => {
-    
-  }
 
+  const emailIsValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  const passIsValid = /^.{6,}$/.test(password); 
   return (
     <form>
       <h1>Login</h1>
       <label htmlFor="email">
         Email:
-        <input type="email" data-testid="email-input" id="email" onChange={handleChange} />
+        <input
+          type="email"
+          data-testid="email-input"
+          id="email" value={email}
+          onChange={({target}) => setEmail(target.value)}
+        />
       </label>
       <label htmlFor="password">
         Senha:
-        <input type="password" data-testid="password-input" id="password" />
+        <input
+          type="password"
+          data-testid="password-input"
+          id="password" onChange={({target}) => setPassword(target.value)}
+        />
       </label>
-      <button type="button" data-testid="login-submit-btn">Entrar</button>
+      <button
+        type="button"
+        disabled={!emailIsValid || !passIsValid}
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </button>
     </form>
   );
 }
