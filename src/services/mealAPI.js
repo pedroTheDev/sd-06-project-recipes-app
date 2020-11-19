06b84665ea3a99e7e8bca0982fb635221522f905
isabellavjs <isabellavjs@gmail.com>
2020-11-19 17:31:48 -0300

    Finaliza os endpoints de meal

--- src/services/mealAPI.js
+++ src/services/mealAPI.js
@@ -4,3 +4,6 @@ const MEAL_API = 'https://www.themealdb.com/api/json/v1/1/';
 const filterByIngredientEndpoint = 'filter.php?i=';
 const searchByNameEndpoint = 'search.php?s=';
 const searchByFirstLetterEndpoint = 'search.php?f=';
+const listCategoriesEndpoint = 'list.php?c=list';
+const requestRecipeEndpoint = 'lookup.php?i=';
+const randomRequestEndpoint = 'random.php';
