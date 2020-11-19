809ad3ff7591ab981c26bbb0c986b7113ef235c1
Marthus Sera <marthus2001@gmail.com>
2020-11-19 17:46:26 -0300

    Adding API fetch

--- src/services/mealAPI.js
+++ src/services/mealAPI.js
@@ -4,3 +4,23 @@ const MEAL_API = 'https://www.themealdb.com/api/json/v1/1/';
 const filterByIngredientEndpoint = 'filter.php?i=';
 const searchByNameEndpoint = 'search.php?s=';
 const searchByFirstLetterEndpoint = 'search.php?f=';
+
+export default async function mealAPI(type, key) {
+  let url = '';
+  switch (type) {
+    case 'ingredient':
+      url = `${MEAL_API}${filterByIngredientEndpoint}${key}`;
+      break;
+    case 'search':
+      url = `${MEAL_API}${searchByNameEndpoint}${key}`;
+      break;
+    case 'firstLetter':
+      url = `${MEAL_API}${searchByFirstLetterEndpoint}${key}`;
+      break;
+    default:
+      return null;
+  }
+
+  const data = await fetch(url);
+  return data;
+}
