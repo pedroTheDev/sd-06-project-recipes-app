dd3cee95286482c650f3e0c8b6c9761afd6e3302
Marcelo Pessini <marceloppessini@gmail.com>
2020-11-19 19:56:52 -0300

    requisito11 finalizado

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import MainFood from './pages/MainFood';
 import MainDrink from './pages/MainDrink';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={Login} />
         <Route exact path="/comidas" component={MainFood} />
         <Route exact path="/bebidas" component={MainDrink} />
+        <Route exact path="/perfil" component={Profile} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function HeaderFood() {
   return (
     <div>
-      <img data-testid="profile-top-btn" src={profileIcon} alt="profile" />
+      <Link to="/perfil"><img data-testid="profile-top-btn" src={profileIcon} alt="profile" /></Link>
       <h1 data-testid="page-title">Comidas</h1>
       <img data-testid="search-top-btn" src={searchIcon} alt="search" />
     </div>
