0b761171e6fd48cc414e351facf87535138c780f
Kesley Lahoz <namielahoz@gmail.com>
2020-11-19 19:07:14 -0300

    Grupo 13 - Arrumado lint

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import mealsContext from './MealsContext';
-import { getAllDrinkTypesApi} from '../services/drinksAPI';
+import { getAllDrinkTypesApi } from '../services/drinksAPI';
 import { getAllRecipeTypesApi } from '../services/mealsAPI';
 
 function MyProvider({ children }) {
