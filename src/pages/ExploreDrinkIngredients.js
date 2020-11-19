398e68402ddcc54b610d1921c8b6d9cad429ed25
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-11-19 20:15:42 -0300

    Requisito9 feito, requisito 10 em construçao

---
+++ src/pages/ExploreDrinkIngredients.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ExploreDrinkIngredients() {
+  return (
+    <div>ExploreDrinkIngredients</div>
+  );
+}
+
+export default ExploreDrinkIngredients;
---
+++ src/pages/ExploreFoodIngredients.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ExploreFoodIngredients() {
+  return (
+    <div>ExploreFoodIngredients</div>
+  );
+}
+
+export default ExploreFoodIngredients;
