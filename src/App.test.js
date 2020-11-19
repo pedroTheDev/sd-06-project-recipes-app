a0ad1fb1bfd6bd87af43528207747659817fbe91
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-11-19 17:11:43 -0300

    Requisito 2: implementando os testes ainda

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import renderWithRouter from './tests/renderWithRouter';
 import App from './App';
 
-test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('Farewell, front-end', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/TRYBE/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('Tela de Login - todos os elementos devem respeitar os atributos', () => {
+  beforeEach(cleanup);
+
+  it('O input email deve possuir o atributo data-testid correto', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+    const email = getByTestId('email-input');
+    const password = getByTestId('password-input');
+    const button = getByTestId('login-submit-btn');
+
+    expect(email).toBeInTheDocument('');
+    expect(password).toBeInTheDocument('');
+    expect(button).toBeInTheDocument('');
+    expect(button.value).toBe('Entrar')
+  });
+})
---
+++ src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <form>
+      <label htmlFor="email">
+        Email:
+        <input type="email" data-testid="email-input" id="email" />
+      </label>
+      <label htmlFor="password">
+        Senha:
+        <input type="password" data-testid="password-input" id="password" />
+      </label>
+      <button type="button" data-testid="login-submit-btn" >Entrar</button>
+    </form>
+  )
+}
+
+export default Login;
