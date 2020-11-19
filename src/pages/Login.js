9d4945c64ce36429426bad6b94b5fd84e524c869
Forna <fornagieri.artur@gmail.com>
2020-11-19 16:16:44 -0300

    Requisito 2 fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,11 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      Login Page
+      <input type="email" data-testid="email-input" />
+      <input type="password" data-testid="password-input" />
+      <button type="button" data-testid="login-submit-btn">
+        Login
+      </button>
     </div>
   );
 }
