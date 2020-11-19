3c69854ed1d13134d3a938375801ce34a73d6047
alcantara2509 <alcantara2509@gmail.com>
2020-11-19 17:59:48 -0300

    [Grupo 22] requisito 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [isValid, setIsValid] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleValidateFields = () => {
+    const minLength = 5;
+    const validateFields = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+    && password.length > minLength;
+    return validateFields;
+  };
+
+  const handleChangeEmail = ({ target: { value } }) => {
+    setEmail(value);
+  };
+
+  const handleChangePassword = ({ target: { value } }) => {
+    setPassword(value);
+    if (handleValidateFields()) setIsValid(true);
+  };
+
   return (
     <>
-      <input type="email" data-testid="email-input" />
-      <input type="password" data-testid="password-input" />
-      <input type="submit" data-testid="login-submit-btn" />
+      <input
+        type="email"
+        name="email"
+        data-testid="email-input"
+        onChange={handleChangeEmail}
+      />
+      <input
+        type="password"
+        name="password"
+        data-testid="password-input"
+        onChange={handleChangePassword}
+      />
+      <input
+        type="submit"
+        data-testid="login-submit-btn"
+        disabled={!isValid}
+      />
     </>
   );
 }
