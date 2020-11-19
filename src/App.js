115939823bbe1372a32d3cc37be3199c237692c7
DehSartoreto <fixsartoreto@gmail.com>
2020-11-19 17:46:55 -0300

    Tela de login finalizada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Provider from './context/Provider';
+import Login from './components/Login';
 
 function App() {
   return (
     <Provider>
-      teste
+      <Switch>
+        <Route exact path="/" component={Login} />
+      </Switch>
     </Provider>
   );
 }
