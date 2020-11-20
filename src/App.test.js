5b295dd3c9bb9a36cede52e42d01f6d9a10157e4
Renan Cunha <renan2610@gmail.com>
2020-11-20 16:10:43 -0300

    Iniciando testes da pagina de Login

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,160 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render } from '@testing-library/react';
+import Login from './Pages/Login'
 
-test('Farewell, front-end', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('2 - Crie todos os elementos que devem respeitar os atributos descritos no protótipo para a tela de login', () => {
+  it('Tem os data-testids email-input, password-input e login-submit-btn', () => {
+    const { getByTestId } = render(<Login/>);
+    const emailInput = getByTestId('email-input');
+    const passwordInput = getByTestId('password-input');
+    const button = getByTestId('login-submit-btn');
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
 });
+
+describe('3 - Desenvolva a tela de maneira que a pessoa deve conseguir escrever seu email no input de email', () => {
+  it('É possível escrever o email', () => {
+    const { getByTestId } = render(<Login/>);
+    const emailInput = getByTestId('email-input');
+
+    fireEvent.change(emailInput, { target: { value: 'email@email.com' } })
+    expect(emailInput.value).toBe('email@email.com')
+  });
+});
+
+describe('4 - Desenvolva a tela de maneira que a pessoa deve conseguir escrever sua senha no input de senha', () => {
+  it('É possível escrever a senha', () => {
+    const { getByTestId } = render(<Login/>);
+    const passwordInput = getByTestId('password-input');
+
+    fireEvent.change(passwordInput, { target: { value: '123456' } })
+    expect(passwordInput.value).toBe('123456')
+  });
+});
+
+describe('5 - Desenvolva a tela de maneira que o formulário só seja válido após um email válido e uma senha de mais de 6 caracteres serem preenchidos', () => {
+  it('O botão deve estar desativado se o email for inválido', () => {
+    const { getByTestId, getByText } = render(<Login/>);
+    const emailInput = getByTestId('email-input');
+    const passwordInput = getByTestId('password-input');
+    const button = getByTestId('login-submit-btn');
+
+    expect(button).toBeDisabled();
+
+    fireEvent.change(emailInput, { target: { value: 'email@email' } })
+    fireEvent.change(passwordInput, { target: { value: '1234567' } })
+
+    expect(button).not.toBeDisabled();
+
+    // fireEvent.change(emailInput, { target: { value: 'email@email' } })
+    // fireEvent.change(passwordInput, { target: { value: '1234567' } })
+    
+
+    // cy.get('[data-testid="email-input"]').clear().type('email.com');
+
+    // cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+  });
+
+  it('O botão deve estar desativado se a senha deve tiver 6 caracteres ou menos', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+
+    cy.get('[data-testid="email-input"]').type('email@mail.com');
+    cy.get('[data-testid="password-input"]').type('123456');
+
+    cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+  });
+
+  it('O botão deve estar ativado se o email e a senha forem válidos', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+
+    cy.get('[data-testid="email-input"]').type('email@mail.com');
+    cy.get('[data-testid="password-input"]').type('1234567');
+
+    cy.get('[data-testid="login-submit-btn"]').should('not.be.disabled');
+  });
+});
+
+// describe('6 - Salve 2 tokens no localStorage após a submissão, identificados pelas chaves mealsToken e cocktailsToken', () => {
+//   it('Após a submissão mealsToken e cocktailsToken devem estar salvos em localStorage', () => {
+//     cy.visit('http://localhost:3000/', {
+//       onBeforeLoad(win) {
+//         win.localStorage.clear();
+//       },
+//     });
+
+//     cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+//     cy.window().then((win) => {
+//       expect(win.localStorage.getItem('mealsToken')).to.be.null;
+//       expect(win.localStorage.getItem('cocktailsToken')).to.be.null;
+//     });
+
+
+//     cy.get('[data-testid="email-input"]').type('email@mail.com');
+//     cy.get('[data-testid="password-input"]').type('1234567');
+//     cy.get('[data-testid="login-submit-btn"]').click();
+
+//     cy.window().then((win) => {
+//       expect(win.localStorage.getItem('mealsToken')).to.eq('1');
+//       expect(win.localStorage.getItem('cocktailsToken')).to.eq('1');
+//       win.localStorage.clear();
+//     });
+//   });
+// });
+
+// describe('7 - Salve o e-mail da pessoa usuária no localStorage na chave user após a submissão', () => {
+//   it('Após a submissão a chave user deve estar salva em localStorage', () => {
+//     cy.visit('http://localhost:3000/', {
+//       onBeforeLoad(win) {
+//         win.localStorage.clear();
+//       },
+//     });
+
+//     cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+//     cy.window().then((win) => {
+//       expect(win.localStorage.getItem('user')).to.be.null;
+//     });
+
+
+//     cy.get('[data-testid="email-input"]').type('email@mail.com');
+//     cy.get('[data-testid="password-input"]').type('1234567');
+//     cy.get('[data-testid="login-submit-btn"]').click();
+
+//     cy.window().then((win) => {
+//       expect(JSON.parse(win.localStorage.getItem('user'))).to.deep.eq({ email: 'email@mail.com' });
+//       win.localStorage.clear();
+//     });
+//   });
+// });
+
+// describe('8 - Redirecione a pessoa usuária para a tela principal de receitas de comidas após a submissão e validação com sucesso do login', () => {
+//   it('A rota muda para a tela principal de receitas de comidas', () => {
+//     cy.visit('http://localhost:3000/', {
+//       onBeforeLoad(win) {
+//         win.localStorage.clear();
+//       },
+//     });
+
+//     cy.get('[data-testid="login-submit-btn"]').should('be.disabled');
+//     cy.window().then((win) => {
+//       expect(win.localStorage.getItem('user')).to.be.null;
+//     });
+
+
+//     cy.get('[data-testid="email-input"]').type('email@mail.com');
+//     cy.get('[data-testid="password-input"]').type('1234567');
+//     cy.get('[data-testid="login-submit-btn"]').click();
+
+//     cy.location().should((loc) => expect(loc.pathname).to.eq('/comidas'));
+
+//     cy.window().then((win) => {
+//       win.localStorage.clear();
+//     });
+//   });
+// });
