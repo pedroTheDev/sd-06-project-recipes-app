9acdbc63ee6102b0aaa24cf25094bb7551481548
Rebeca C Santos <54561040+us19861229c@users.noreply.github.com>
2020-11-20 17:01:41 -0300

    Eslint - Perfil.jsx

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -7,14 +7,14 @@ export default function Perfil() {
   const { titulo, setTitulo } = useContext(Context);
 
   useEffect(() => {
-    setTitulo('Perfil')
+    setTitulo('Perfil');
   }, []);
 
   return (
     <div>
-      <Header titulo={ titulo } />
+      <Header titulo={titulo} />
       <h1>{ titulo }</h1>
-      <p data-testid="profile-email"></p>
+      <p data-testid="profile-email">email</p>
       <Link to="/receitas-feitas"><button type="button" data-testid="profile-done-btn">Receitas Feitas</button></Link>
       <Link to="/receitas-favoritas"><button type="button" data-testid="profile-favorite-btn">Receitas Favoritas</button></Link>
       <Link to="/"><button type="button" data-testid="profile-logout-btn">Sair</button></Link>
