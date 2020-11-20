cd8d44dc4a7c0af25b89db1c0a456ad2f7904729
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-11-20 16:02:11 -0300

    Requisito 9 + css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import profileIcon from "../images/profileIcon.svg";
+import searchIcon from "../images/searchIcon.svg";
+import "./Header.css";
 
 const Header = () => (
-  <div />
+  <section className="header">
+    <Link to="" data-testid="profile-top-btn" className="image">
+      <img src={profileIcon} />
+    </Link>
+    <h1 data-testids="page-title">PageTitle</h1>
+    <Link to="" data-testids="search-top-btn" className="image">
+      <img src={searchIcon} />
+    </Link>
+  </section>
 );
 
 export default Header;
