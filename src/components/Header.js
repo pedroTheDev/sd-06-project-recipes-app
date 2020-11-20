df3750e4725a2f37150f9535f7e0b9dcf3df9311
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-11-20 13:12:04 -0300

    mudanças

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import SearchBar from './SearchBar';
 
 function HeaderFood() {
   return (
@@ -8,6 +9,7 @@ function HeaderFood() {
       <img data-testid="profile-top-btn" src={profileIcon} alt="profile" />
       <h1 data-testid="page-title">Comidas</h1>
       <img data-testid="search-top-btn" src={searchIcon} alt="search" />
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,11 +9,12 @@ export default function SearchBar() {
         <br />
         <input type="radio" value="ingrediente" data-testid="ingredient-search-radio" />
         <label htmlFor="ingrediente">Ingrediente</label>
-        <input type="radio" value="nome" />
+        <input type="radio" value="nome" data-testid="name-search-radio" />
         <label htmlFor="nome">Nome</label>
         <input type="radio" value="primeira letra" data-testid="first-letter-search-radio" />
         <label htmlFor="primeira letra">Primeira Letra</label>
       </form>
+      <button type="button" data-testid="exec-search-btn">Executa Busca</button>
     </div>
   );
 }
