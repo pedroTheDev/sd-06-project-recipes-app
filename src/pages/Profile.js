4699210379eb9de098a71cc8d8b9367794407bf5
Marcelo Pessini <marceloppessini@gmail.com>
2020-11-20 11:33:30 -0300

    Requisito10 Comcluido + Todas as rodas adicionadas

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function MainFood() {
   return (
     <div>
-      <h1 data-testid="page-title">Perfil</h1>
+      <Header />
     </div>
   );
 }
