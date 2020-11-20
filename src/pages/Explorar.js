98939bcf7e7250a75b9a941f8a5fa7c7929ab3a5
Elisa <elisatcamillo@gmail.com>
2020-11-20 17:08:12 -0300

    requisitos 67-69

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Explorar = () => (
-  <div />
+  <div>
+    <Link to="/explorar/comidas" data-testid="explore-food">
+      <span>Explorar Comidas</span>
+    </Link>
+    <Link to="/explorar/bebidas" data-testid="explore-drinks">
+      <span>Explorar Bebidas</span>
+    </Link>
+  </div>
 );
 
 export default Explorar;
