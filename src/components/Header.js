e1b6f2f325e1d426dd2fa9304632845f21b91d62
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-11-20 16:35:29 -0300

    refactoring req 9

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import profileIcon from "../images/profileIcon.svg";
-import searchIcon from "../images/searchIcon.svg";
-import "./Header.css";
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
+import './Header.css';
 
-const Header = () => (
-  <section className="header">
-    <Link to="" data-testid="profile-top-btn" className="image">
-      <img src={profileIcon} />
-    </Link>
-    <h1 data-testids="page-title">PageTitle</h1>
-    <Link to="" data-testids="search-top-btn" className="image">
-      <img src={searchIcon} />
-    </Link>
-  </section>
-);
+const Header = () => {
+  return (
+    <section className="header">
+      <Link to="/perfil" >
+        <button data-testid="profile-top-btn" className="image">
+          <img src={profileIcon} alt="Profile button" />
+        </button>
+      </Link>
+      <h1 data-testids="page-title">{window.location.pathname.replace(/[^a-zA-Z0-9]/g, "")}</h1>
+      <button data-testids="search-top-btn" className="image">
+        <img src={searchIcon} alt="show-hide-sbr" />
+      </button>
+    </section>
+  )
+};
 
 export default Header;
--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const Perfil = () => (
-  <div />
+  <Header />
 );
 
 export default Perfil;
