277072f40e17fc242b12b79d4200383b18ec7751
Ilan Ribeiro <67593192+ilanribeiro@users.noreply.github.com>
2020-11-20 16:57:16 -0300

    Eslint - Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,14 +5,14 @@ import Lupa from './Lupa';
 import profileImage from '../images/profileIcon.svg';
 
 export default function Header() {
-  const { titulo, setTitulo } = useContext(Context);
+  const { titulo } = useContext(Context);
 
   return (
     <header className="header">
       <div>
         <Link to="/perfil"><button type="button" data-testid="profile-top-btn" src="../images/profileIcon.svg" alt="profile"><img src={profileImage} alt="profile" /></button></Link>
-			   <h1 data-testid="page-title">{ titulo }</h1>
-         <Lupa />
+	<h1 data-testid="page-title">{ titulo }</h1>
+        <Lupa />
       </div>
     </header>
   );
