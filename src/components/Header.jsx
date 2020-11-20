8ebcc7481f81b394cc87e65c2f594fdb527652a9
lucasvr39 <lucasveras1994@gmail.com>
2020-11-20 16:29:38 -0300

    implement req 11

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header({name}) {
   return (
     <div>
-      <button className="logo-profile" data-testid="profile-top-btn">profile-icon</button>
+      <Link to="/perfil">
+        <button className="logo-profile" data-testid="profile-top-btn">profile-icon</button>
+      </Link>
       <h1 data-testid="page-title">{name}</h1>
       <button className="logo-search" data-testid="search-top-btn">search-icon</button>
     </div>
