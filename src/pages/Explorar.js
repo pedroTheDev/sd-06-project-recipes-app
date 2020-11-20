ece54faf1a48eaa4a0023064e152e6a04fe202e7
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-11-20 18:30:04 -0300

    lint

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -2,16 +2,18 @@ import React from 'react';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
 
-const Explorar = () => (
-  <div>
-    <Link to="/explorar/comidas" data-testid="explore-food">
-      <span>Explorar Comidas</span>
-    </Link>
-    <Link to="/explorar/bebidas" data-testid="explore-drinks">
-      <span>Explorar Bebidas</span>
-    </Link>
-    <Footer />
-  </div>
-);
+const Explorar = () => {
+  return (
+    <div>
+      <Link to="/explorar/comidas" data-testid="explore-food">
+        <span>Explorar Comidas</span>
+      </Link>
+      <Link to="/explorar/bebidas" data-testid="explore-drinks">
+        <span>Explorar Bebidas</span>
+      </Link>
+      <Footer />
+    </div>
+  );
+};
 
 export default Explorar;
