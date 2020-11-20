5d2255e9e70cd34d8721b82ecefc07783fecfc16
danieleperse <danieleperse3@gmail.com>
2020-11-20 18:01:36 -0300

    footer finalizado

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import { fetchAPIDrinks } from '../services';
 
 function Drinks() {
   return (
     <div>
       <Header title="Bebidas" fetchApi={ fetchAPIDrinks } />
+      <Footer />
     </div>
   );
 }
