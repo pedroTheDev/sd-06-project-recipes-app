74fd099b2ead6e47b89fcc9f98d7564ef396e2cb
alcantara2509 <alcantara2509@gmail.com>
2020-11-19 21:08:50 -0300

    Requirement 19, footermenu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 export default function Footer() {
   return (
-    <div className="footer">
+    <div className="footer" data-testid="footer">
       <div />
+      <Link to="/bebidas" data-testid="drinks-bottom-btn">
+        <img src="./images/drinkIcon.svg" alt="Drink icon" />
+      </Link>
+      <Link to="/explorar" data-testid="explore-bottom-btn">
+        <img src="./images/exploreIcon.svg" alt="exploreIcon" />
+      </Link>
+      <Link to="/comida" data-testid="food-bottom-btn">
+        <img src="./images/mealIcon.svg" alt="mealIcon" />
+      </Link>
     </div>
   );
 }
---
+++ src/pages/Food.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+function Food() {
+  return (
+    <>
+      <h1>Food</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default Food;
