49cdbfee350a83a5d18b114c26574015913e7844
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-11-20 15:10:46 -0300

    :rotating_light: Fixing some lint issues

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Login from './pages/Login';
-import Comidas from './pages/Comidas';
+// import Comidas from './pages/Comidas';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+// import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Route exact path='/comidas' component={ Comidas } />
-        <Route exact path='/' component={ Login } />
-      </BrowserRouter>
+      <Login />
     </div>
   );
 }
