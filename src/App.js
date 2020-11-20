20eb4afd6b362f01162b8e961e6282e0929c3f70
Isadora Koga <isadorakoga710@gmail.com>
2020-11-20 17:18:22 -0300

    :rotating_light: Fix linter

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import ExplorarBebidasIngredientes from './Pages/ExplorarBebidasIngredientes';
 import ExplorarComidasArea from './Pages/ExplorarComidasArea';
 import Perfil from './Pages/Perfil';
 import ComidasInProgress from './Pages/ComidasInProgress';
-import BebidasInProgress from './Pages/BebidasInProgress.jsx';
+import BebidasInProgress from './Pages/BebidasInProgress';
 import ReceitasFeitas from './Pages/ReceitasFeitas';
 import ReceitasFavoritas from './Pages/ReceitasFavoritas';
 
@@ -23,15 +23,27 @@ function App() {
       <Route exact path="/" component={ Login } />
       <Route exact path="/comidas" component={ Comidas } />
       <Route exact path="/bebidas" component={ Bebidas } />
-      <Route path="/comidas/:id" render={(props) => <ComidasDetails {...props} />} />
-      <Route path="/bebidas/:id" render={(props) => <BebidasDetails {...props} />} />
-      <Route path="/comidas/:id/in-progress" render={(props) => <ComidasInProgress {...props} />} />
-      <Route path="/bebidas/:id/in-progress" render={(props) => <BebidasInProgress {...props} />} />
+      <Route path="/comidas/:id" render={ (props) => <ComidasDetails { ...props } /> } />
+      <Route path="/bebidas/:id" render={ (props) => <BebidasDetails { ...props } /> } />
+      <Route
+        path="/comidas/:id/in-progress"
+        render={ (props) => <ComidasInProgress { ...props } /> }
+      />
+      <Route
+        path="/bebidas/:id/in-progress"
+        render={ (props) => <BebidasInProgress { ...props } /> }
+      />
       <Route path="/explorar" component={ Explorar } />
       <Route path="/explorar/comidas" component={ ExplorarComidas } />
       <Route path="/explorar/bebidas" component={ ExplorarBebidas } />
-      <Route path="/explorar/comidas/ingredientes" component={ ExplorarComidasIngredientes } />
-      <Route path="/explorar/bebidas/ingredientes" component={ ExplorarBebidasIngredientes } />
+      <Route
+        path="/explorar/comidas/ingredientes"
+        component={ ExplorarComidasIngredientes }
+      />
+      <Route
+        path="/explorar/bebidas/ingredientes"
+        component={ ExplorarBebidasIngredientes }
+      />
       <Route path="/explorar/comidas/area" component={ ExplorarComidasArea } />
       <Route path="/perfil" component={ Perfil } />
       <Route path="/receitas-feitas" component={ ReceitasFeitas } />
