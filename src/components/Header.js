4ee09660dc8fe39866ff60c77f55dc3b629ef551
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2020-11-20 17:31:18 -0300

    Header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
-function Header() {
+function Header({ title }) {
   return (
     <header>
-      <button data-testid="profile-top-btn" type="button">Perfil</button>
-      <h2 data-testid="page-title">Comidas</h2>
-      <button data-testid="search-top-btn" type="button">Pesquisar</button>
+      <button
+        data-testid="profile-top-btn"
+        type="button"
+      >
+        <img src={ searchIcon } alt="Search" />
+      </button>
+      <h2 data-testid="page-title">{ title }</h2>
+      <Link
+        to="/perfil"
+        data-testid="search-top-btn"
+        type="button"
+      >
+        <img
+          src={ profileIcon }
+          alt="Profile"
+        />
+      </Link>
     </header>
   );
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
