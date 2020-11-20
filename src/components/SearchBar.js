9c109e1fbe2580028a856215e28342c995de06e8
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2020-11-20 15:19:22 -0300

    lint

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,15 +4,33 @@ export default function SearchBar() {
   return (
     <div className="SearchBar">
       <form>
-        <label>Barra de Busca: </label>
-        <input type="text" data-testid="search-input" />
+        <label htmlFor="Busca">
+          Barra de Busca:
+          <input id="Busca" type="text" data-testid="search-input" />
+        </label>
         <br />
-        <input type="radio" value="ingrediente" data-testid="ingredient-search-radio" />
-        <label htmlFor="ingrediente">Ingrediente</label>
-        <input type="radio" value="nome" data-testid="name-search-radio" />
-        <label htmlFor="nome">Nome</label>
-        <input type="radio" value="primeira letra" data-testid="first-letter-search-radio" />
-        <label htmlFor="primeira letra">Primeira Letra</label>
+        <label htmlFor="ingrediente">
+          Ingrediente
+          <input
+            type="radio"
+            value="ingrediente"
+            id="ingrediente"
+            data-testid="ingredient-search-radio"
+          />
+        </label>
+        <label htmlFor="nome">
+          Nome
+          <input type="radio" value="nome" data-testid="name-search-radio" />
+        </label>
+        <label htmlFor="primeira letra">
+          <input
+            id="nome"
+            type="radio"
+            value="primeira letra"
+            data-testid="first-letter-search-radio"
+          />
+          Primeira Letra
+        </label>
       </form>
       <button type="button" data-testid="exec-search-btn">Executa Busca</button>
     </div>
