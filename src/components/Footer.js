b09c5eca53b148646db4f21d4a113853eb220bfc
alcantara2509 <alcantara2509@gmail.com>
2020-11-19 21:14:05 -0300

    Requirement 20, link icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 export default function Footer() {
   return (
     <div className="footer" data-testid="footer">
       <div />
-      <Link to="/bebidas" data-testid="drinks-bottom-btn">
-        <img src="./images/drinkIcon.svg" alt="Drink icon" />
+      <Link to="/bebidas" data-testid="drinks-bottom-btn" src={drinkIcon}>
+        <img src={drinkIcon} alt="Drink icon" />
       </Link>
-      <Link to="/explorar" data-testid="explore-bottom-btn">
-        <img src="./images/exploreIcon.svg" alt="exploreIcon" />
+      <Link to="/explorar" data-testid="explore-bottom-btn" src={exploreIcon}>
+        <img src={exploreIcon} alt="exploreIcon" />
       </Link>
-      <Link to="/comida" data-testid="food-bottom-btn">
-        <img src="./images/mealIcon.svg" alt="mealIcon" />
+      <Link to="/comida" data-testid="food-bottom-btn" src={mealIcon}>
+        <img src={mealIcon} alt="mealIcon" />
       </Link>
     </div>
   );
