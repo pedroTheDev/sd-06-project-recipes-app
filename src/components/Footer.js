6ba433c0bd868c6afaf2d958b408cea9185ce94d
Danilo <dansantosalfa@gmail.com>
2020-11-20 17:03:21 -0300

    Lint

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,22 @@ function Footer() {
     <div data-testid="footer">
       <form className="Footer">
         <Link to="/bebidas">
-          <button type="button" data-testid="drinks-bottom-btn"><img src={"./src/imagens/drinkIcon.svg"} alt="DRINKS"/></button>
+          <button type="button" data-testid="drinks-bottom-btn">
+            <img src={ "./src/imagens/drinkIcon.svg" } alt="DRINKS" />
+          </button>
         </Link>
         <Link to="/explore">
-          <button type="button" data-testid="explore-bottom-btn"><img src={"./src/imagens/exploreIcon.svg"}alt="EXPLORE"/></button>
+          <button type="button" data-testid="explore-bottom-btn">
+            <img src={ "./src/imagens/exploreIcon.svg" } alt="EXPLORE" />
+          </button>
         </Link>
         <Link to="/comidas">
-          <button type="button" data-testid="food-bottom-btn"> <img src={"./src/imagens/mealIcon.svg"} alt="FOOD"/> </button>
+          <button type="button" data-testid="food-bottom-btn">
+            <img src={ "./src/imagens/mealIcon.svg" } alt="FOOD" />
+          </button>
         </Link>
       </form>
-      
+
     </div>
   );
 }
