327219ddc647a7e9059bcc9d18f5f4de83537b78
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-11-20 20:36:27 -0300

    linter1

--- src/components/BotoesFiltrar.js
+++ src/components/BotoesFiltrar.js
@@ -1,32 +1,32 @@
-import React, { useContext } from 'react';
-import { categoriesApi } from '../service/foodApi';
-import ReceitasContext from '../context/ReceitasContext';
+// import React, { useContext } from 'react';
+// import { categoriesApi } from '../service/foodApi';
+// import ReceitasContext from '../context/ReceitasContext';
 
-const BotoesFiltrar = () => {
-  const { filtersData, setFiltersData } = useContext(ReceitasContext);
+// const BotoesFiltrar = () => {
+//   // const { filtersData, setFiltersData } = useContext(ReceitasContext);
 
-  function teste() {
-    categoriesApi().then((response) => {});
-  }
-  teste();
+//   // function teste() {
+//   //   categoriesApi().then((response) => {});
+//   // }
+//   // teste();
 
-  return (
-    <div>
-      {/* {filtersData.map((categ, i) =>
-        i <= 4 ? (
-          <button
-            type="button"
-            key={categ.strCategory}
-            data-testid={`${categ.strCategory}-category-filter`}
-            onClick={handleClick}
-            value={categ.strCategory}
-          >
-            {categ.strCategory}
-          </button>
-        ) : null
-      )} */}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {/* {filtersData.map((categ, i) =>
+//         i <= 4 ? (
+//           <button
+//             type="button"
+//             key={categ.strCategory}
+//             data-testid={`${categ.strCategory}-category-filter`}
+//             onClick={handleClick}
+//             value={categ.strCategory}
+//           >
+//             {categ.strCategory}
+//           </button>
+//         ) : null
+//       )} */}
+//     </div>
+//   );
+// };
 
-export default BotoesFiltrar;
+// export default BotoesFiltrar;
--- src/components/ComidaCard.js
+++ src/components/ComidaCard.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 
 function ComidaCard({ food, index }) {
   return (
-    <div data-testid={`${index}-recipe-card`}>
+    <div data-testid={ `${index}-recipe-card` }>
       <img
-        data-testid={`${index}-card-img`}
-        src={food.strMealThumb}
-        alt={food.strMeal}
+        data-testid={ `${index}-card-img` }
+        src={ food.strMealThumb }
+        alt={ food.strMeal }
       />
-      <h3 data-testid={`${index}-card-name`}>{food.strMeal}</h3>
+      <h3 data-testid={ `${index}-card-name` }>{food.strMeal}</h3>
     </div>
   );
 }
