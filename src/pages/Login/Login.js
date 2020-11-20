2426a331ce160b18b66b1be92ec5ac97293db1a5
Bruna Franciele <brunafranciele1902@gmail.com>
2020-11-20 15:36:13 -0300

    login

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState(false);
+  const [password, setPassword] = useState(false);
+  const [user, setUser] = useState('');
+
+  function verifyAndSetEmail(param) {
+    const emailFormat = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/.test(param);
+    setUser(param);
+    return setEmail(emailFormat);
+  }
+
+  function verifyAndSetPassword(param) {
+    const minPasswordLength = 6;
+    let verify = false;
+    if (param.length >= minPasswordLength) {
+      verify = true;
+    }
+    return setPassword(verify);
+  }
+
+  return (
+    <div>
+      <form>
+        <div className="form-group">
+          <label
+            htmlFor="email"
+          >
+            Email
+      </label>
+          <input
+            className="form-control"
+            type="email"
+            data-testid="email-input"
+            placeholder="Digite seu email"
+            id="password"
+            onChange={ ({ target }) => verifyAndSetEmail(target.value) }
+            required="required"
+            value={ user }
+          />
+        </div>
+        <div className="form-group">
+          <label
+            htmlFor="senha"
+          >
+            Senha
+      </label>
+          <input
+            className="form-control"
+            type="password"
+            data-testid="password-input"
+            placeholder="Digite sua senha"
+            id="senha"
+            onChange={ ({ target }) => verifyAndSetPassword(target.value) }
+            required="required"
+          />
+        </div>
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+          className="btn btn-primary"
+          disabled={ !email + !password }
+        >
+          Entrar
+        </button>
+      </form >
+    </div>
+  )
+}
+
+export default Login;
