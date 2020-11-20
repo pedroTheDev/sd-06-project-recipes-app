768c72b613ab783fbfd844e7ab98b935b42334a8
LeandroParisi <lepcbelisario@gmail.com>
2020-11-20 09:46:30 -0300

    ESLint 2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-curly-spacing */
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
@@ -12,24 +13,28 @@ import store from './redux/store';
 
 function App() {
   return (
-    <Provider store={store}>
+    <Provider store={ store }>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/bebidas" component={Bebidas} />
-          <Route exact path="/bebidas/:id" component={BebidasDetalhes} />
-          <Route exact path="/bebidas/:id/in-progress" component={BebidasProgresso} />
-          <Route exact path="/explorar/bebidas/ingredientes" component={BebidasIngredientes} />
-          <Route exact path="/comidas" component={Comidas} />
-          <Route exact path="/comidas/:id" component={ComidasDetalhes} />
-          <Route exact path="/comidas/:id/in-progress" component={ComidasProgresso} />
-          <Route exact path="/explorar/comidas/ingredientes" component={ComidasIngredientes} />
-          <Route exact path="/explorar/comidas/area" component={ComidasArea} />
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/bebidas" component={ Bebidas } />
+          <Route exact path="/bebidas/:id" component={ BebidasDetalhes } />
+          <Route exact path="/bebidas/:id/in-progress" component={ BebidasProgresso } />
+          <Route
+            exact
+            path="/explorar/bebidas/ingredientes"
+            component={ BebidasIngredientes }
+          />
+          <Route exact path="/comidas" component={ Comidas } />
+          <Route exact path="/comidas/:id" component={ ComidasDetalhes } />
+          <Route exact path="/comidas/:id/in-progress" component={ ComidasProgresso } />
+          <Route exact path="/explorar/comidas/ingredientes" component={ ComidasIngredientes } />
+          <Route exact path="/explorar/comidas/area" component={ ComidasArea } />
           <Route exact path="/explorar" component={Explorar} />
-          <Route exact path="/explorar/comidas" component={ExplorarComidas} />
-          <Route exact path="/explorar/bebidas" component={ExplorarBebidas} />
-          <Route exact path="/receitas-feitas" component={ReceitasFeitas} />
-          <Route exact path="/receitas-favoritas" component={ReceitasFavoritas} />
+          <Route exact path="/explorar/comidas" component={ ExplorarComidas } />
+          <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
+          <Route exact path="/receitas-feitas" component={ ReceitasFeitas } />
+          <Route exact path="/receitas-favoritas" component={ ReceitasFavoritas } />
           <Route exact path="/perfil" component={Perfil} />
 
         </Switch>
