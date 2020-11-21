c774813595d4c962edb0b93e68f7513587c23531
Paulo Frazão <paulo4187@gmail.com>
2020-11-21 16:38:34 -0300

    update conflicts branchs merge

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,21 @@
 // import triviaAPI from '../services/triviaAPI';
 
 export const USER_INFO = 'USER_INFO';
+export const DRINKS = 'DRINKS';
+export const MEALS = 'MEALS';
 
 export const UserInfo = (email, password) => ({
   type: USER_INFO,
   email,
   password,
+});
+
+export const comida = (meal) => ({
+  type: MEALS,
+  meal,
+});
+
+export const bebida = (drink) => ({
+  type: DRINKS,
+  drink,
 });
--- src/services/FetchAPI.js
+++ src/services/FetchAPI.js
@@ -1,4 +1,4 @@
-function fetchMeal(endPoint, id) {
+export function fetchMeal(endPoint, id) {
   let variavel;
   if (id === 'ingrediente') {
     variavel = fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${endPoint}`)
@@ -16,4 +16,24 @@ function fetchMeal(endPoint, id) {
   return variavel;
 }
 
-export default fetchMeal;
+export function fetchDrinks(endPoint, value) {
+  let variavel;
+  if (endPoint.length < 1) {
+    variavel = fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=lemon')
+      .then((response) => response.json())
+      .then((response) => response.drinks);
+  } else if (value === 'ingrediente') {
+    variavel = fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${endPoint}`)
+      .then((response) => response.json())
+      .then((response) => response.drinks);
+  } else if (value === 'nome') {
+    variavel = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${endPoint}`)
+      .then((response) => response.json())
+      .then((response) => response.drinks);
+  } else if (value === 'primeira-letra') {
+    variavel = fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${endPoint}`)
+      .then((response) => response.json())
+      .then((response) => response.drinks);
+  }
+  return variavel;
+}
