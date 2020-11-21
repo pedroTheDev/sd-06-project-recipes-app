8c10e5ad5d023a83d9fee7a3ab7c3e1e3f774882
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2020-11-20 21:36:52 -0300

    requisito 10 eslint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,9 @@ import profileIcon from '../styles/images/profileIcon.svg';
 import searchIcon from '../styles/images/searchIcon.svg';
 import HeaderSearch from './HeaderSearch';
 
-function Header({name}) {
-  const { contextValue: { searchHeader, setSearchHeader } } = useContext(RecipesAppContext);
+function Header({ name }) {
+  const {
+    contextValue: { searchHeader, setSearchHeader } } = useContext(RecipesAppContext);
 
   const isClick = () => {
     if (searchHeader) {
@@ -13,22 +14,31 @@ function Header({name}) {
     } else {
       setSearchHeader(true);
     }
-  }
+  };
 
   return (
     <div>
       <header>
-        <button className="logo-profile" data-testid="profile-top-btn" >
-          <img src={profileIcon} alt="profile icon" />
+        <button type="button" className="logo-profile" data-testid="profile-top-btn">
+          <img src={ profileIcon } alt="profile icon" />
         </button>
         <h1 data-testid="page-title">{name}</h1>
-        <button className="logo-search" data-testid="search-top-btn" onClick={isClick}>
-        <img src={searchIcon} alt="search icon" />
+        <button
+          type="button"
+          className="logo-search"
+          data-testid="search-top-btn"
+          onClick={ isClick }
+        >
+          <img src={ searchIcon } alt="search icon" />
         </button>
       </header>
-      <HeaderSearch enable={searchHeader} />
+      <HeaderSearch enable={ searchHeader } />
     </div>
   );
 }
+
+Header.propTypes = {
+  name: propTypes.string.isRequired,
+};
 
 export default Header;
