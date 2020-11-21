667cdb79f974750281081d4d044c7ecbfc3fa563
Micaeli Teixeira <luime6@gmail.com>
2020-11-20 21:15:27 -0300

    changes eslint

--- src/App.js
+++ src/App.js
@@ -22,34 +22,34 @@ import {
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route exact path="/comidas" component={Comidas} />
-      <Route exact path="/bebidas" component={Bebidas} />
-      <Route path="/comidas/:id" render={<ComidasDetails />} />
-      <Route path="/bebidas/:id" render={<BebidasDetails />} />
+      <Route exact path="/" component={ Login } />
+      <Route exact path="/comidas" component={ Comidas } />
+      <Route exact path="/bebidas" component={ Bebidas } />
+      <Route path="/comidas/:id" render={ (props) => <ComidasDetails { ...props } /> } />
+      <Route path="/bebidas/:id" render={ (props) => <BebidasDetails { ...props } /> } />
       <Route
         path="/comidas/:id/in-progress"
-        render={<ComidasInProgress />}
+        render={ (props) => <ComidasInProgress { ...props } /> }
       />
       <Route
         path="/bebidas/:id/in-progress"
-        render={<BebidasInProgress />}
+        render={ (props) => <BebidasInProgress { ...props } /> }
       />
-      <Route path="/explorar/comidas/area" component={ExplorarComidasArea} />
+      <Route path="/explorar/comidas/area" component={ ExplorarComidasArea } />
       <Route
         path="/explorar/comidas/ingredientes"
-        component={ExplorarComidasIngredientes}
+        component={ ExplorarComidasIngredientes }
       />
       <Route
         path="/explorar/bebidas/ingredientes"
-        component={ExplorarBebidasIngredientes}
+        component={ ExplorarBebidasIngredientes }
       />
-      <Route path="/explorar/comidas" component={ExplorarComidas} />
-      <Route path="/explorar/bebidas" component={ExplorarBebidas} />
-      <Route path="/explorar" component={Explorar} />
-      <Route path="/perfil" component={Perfil} />
-      <Route path="/receitas-feitas" component={ReceitasFeitas} />
-      <Route path="/receitas-favoritas" component={ReceitasFavoritas} />
+      <Route path="/explorar/comidas" component={ ExplorarComidas } />
+      <Route path="/explorar/bebidas" component={ ExplorarBebidas } />
+      <Route path="/explorar" component={ Explorar } />
+      <Route path="/perfil" component={ Perfil } />
+      <Route path="/receitas-feitas" component={ ReceitasFeitas } />
+      <Route path="/receitas-favoritas" component={ ReceitasFavoritas } />
     </Switch>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -29,16 +29,29 @@ function Header({ pageName }) {
   return (
     <div>
       <div className="header">
-        <Link to="/perfil"><img src={profileIcon} data-testid="profile-top-btn" alt="Profile Icon Button" /></Link>
+        <Link to="/perfil">
+          <img
+            src={ profileIcon }
+            data-testid="profile-top-btn"
+            alt="Profile Icon Button"
+          />
+        </Link>
         <p data-testid="page-title">{pageName}</p>
-        { (pageName !== 'Receitas Feitas' && pageName !== 'Receitas Favoritas' && pageName !== 'Explorar' && pageName !== 'Explorar Comidas' && pageName !== 'Explorar Bebidas' && pageName !== 'Explorar Ingredientes' && pageName !== 'Perfil')
+        { (pageName !== 'Receitas Feitas' && pageName !== 'Receitas Favoritas'
+        && pageName !== 'Explorar' && pageName !== 'Explorar Comidas'
+        && pageName !== 'Explorar Bebidas' && pageName !== 'Explorar Ingredientes'
+        && pageName !== 'Perfil')
           ? (
-            <button className="button-search" type="button" onClick={handleClick}>
-              <img src={searchIcon} data-testid="search-top-btn" alt="Search Icon Button" />
+            <button className="button-search" type="button" onClick={ handleClick }>
+              <img
+                src={ searchIcon }
+                data-testid="search-top-btn"
+                alt="Search Icon Button"
+              />
             </button>
           ) : null}
       </div>
-      <div id="shearBar" className={divShow} />
+      <div id="shearBar" className={ divShow } />
 
     </div>
   );
