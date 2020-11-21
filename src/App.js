efc0c7c46750fc6a43ce4e02a1b91e37b53a3478
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-11-20 22:49:30 -0300

    :sparkles: Main group 23 - Req 14 e 15 - Radio Buttons Logic for meal and Drink requisitions

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes/index';
 import './App.css';
-import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header title="Comidas" />
       <Routes />
     </BrowserRouter>
   );
--- src/context/RecipesAppProvider.js
+++ src/context/RecipesAppProvider.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import RecipesAppContext from './RecipesAppContext';
 
 function RecipesAppProvider({ children }) {
+  const [ingredient, setIngredient] = useState(false);
+  const [firstLetter, setFirstLetter] = useState(false);
+  const [name, setName] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const value = {
+    ingredient,
+    setIngredient,
+    firstLetter,
+    setFirstLetter,
+    name,
+    setName,
+    searchTerm,
+    setSearchTerm,
+  };
+
   return (
-    <RecipesAppContext.Provider value={ {} }>
+    <RecipesAppContext.Provider value={ value }>
       {children}
     </RecipesAppContext.Provider>
   );
