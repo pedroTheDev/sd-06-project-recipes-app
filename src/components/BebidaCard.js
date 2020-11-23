a295cce047f98ed899da3d269c01e97a0a85384b
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-11-23 16:14:11 -0300

    css e item23 sem passar

--- src/components/BebidaCard.js
+++ src/components/BebidaCard.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../style/ReceitasList.css';
 
 function BebidaCard({ drink, index }) {
   return (
-    <div data-testid={ `${index}-recipe-card` }>
+    <div className="drink-card" data-testid={ `${index}-recipe-card` }>
       <img
         data-testid={ `${index}-card-img` }
         src={ drink.strDrinkThumb }
         alt={ drink.strDrink }
+        className="drink-img"
       />
-      <h3 data-testid={ `${index}-card-name` }>{ drink.strDrink }</h3>
+      <h3 className="drink-name" data-testid={ `${index}-card-name` }>
+        {drink.strDrink}
+      </h3>
     </div>
   );
 }
--- src/components/ComidaCard.js
+++ src/components/ComidaCard.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../style/ReceitasList.css';
 
 function ComidaCard({ food, index }) {
   return (
-    <div data-testid={ `${index}-recipe-card` }>
+    <div className="food-card" data-testid={ `${index}-recipe-card` }>
       <img
         data-testid={ `${index}-card-img` }
         src={ food.strMealThumb }
         alt={ food.strMeal }
+        className="food-img"
       />
-      <h3 data-testid={ `${index}-card-name` }>{ food.strMeal }</h3>
+      <h3 className="food-name" data-testid={ `${index}-card-name` }>
+        {food.strMeal}
+      </h3>
     </div>
   );
 }
