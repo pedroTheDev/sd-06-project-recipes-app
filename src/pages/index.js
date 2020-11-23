13d192b9ab3428dc0bfe62f3060c4751383cd65e
Ana Higo <anahigo.arq@gmail.com>
2020-11-23 00:03:27 -0300

    Correçao de Lint 21 Incompleto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
+export { default as Login } from './Login';
+export { default as NotFound } from './NotFound';
 export { default as Bebidas } from './Bebidas';
 export { default as Comidas } from './Comidas';
-export { default as DoneRecipes } from './DoneRecipes';
 export { default as Explorar } from './Explorar';
 export { default as ExplorarComidas } from './ExplorarComidas';
 export { default as ExplorarBebidas } from './ExplorarBebidas';
