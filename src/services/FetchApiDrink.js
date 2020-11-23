20b82cbb8424b10902b5d7c618f319c9830fc92c
Marcelo Pessini <marceloppessini@gmail.com>
2020-11-23 11:36:10 -0300

    refact 14 && 15

--- src/services/FetchApiDrink.js
+++ src/services/FetchApiDrink.js
@@ -1,18 +1,13 @@
 export default async function fetchApiDrink(radioSelected, value, setData) {
+  let endpoint;
   if (radioSelected === '1') {
-    const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${value}`;
-    const response = await fetch(endpoint);
-    const responseJson = await response.json();
-    setData(responseJson);
+    endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${value}`;
   } else if (radioSelected === '2') {
-    const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`;
-    const response = await fetch(endpoint);
-    const responseJson = await response.json();
-    setData(responseJson);
+    endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`;
   } else if (radioSelected === '3') {
-    const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${value}`;
-    const response = await fetch(endpoint);
-    const responseJson = await response.json();
-    setData(responseJson);
+    endpoint = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${value}`;
   }
+  const response = await fetch(endpoint);
+  const responseJson = await response.json();
+  setData(responseJson);
 }
