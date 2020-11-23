ef7e456ed63515722e8cb016d190dad8753b08ba
Renan Cunha <renan2610@gmail.com>
2020-11-23 20:15:46 -0300

    Arrumando erros do linter

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,32 +6,30 @@ import drinkIcon from '../../images/drinkIcon.svg';
 import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
 
-const Footer = () => {
-  return (
-    <div data-testid="footer" className="footer">
-      <Link to="/bebidas">
-        <img
-          data-testid="drinks-bottom-btn"
-          src={drinkIcon}
-          alt="drinkIcon"
-        />
-      </Link>
-      <Link to="/explorar">
-        <img
-          data-testid="profile-top-btn"
-          src={exploreIcon}
-          alt="exploreIcon"
-        />
-      </Link>
-      <Link to="/comidas">
-        <img
-          data-testid="food-bottom-btn"
-          src={mealIcon}
-          alt="mealIcon"
-        />
-      </Link>
-    </div>
-  );
-};
+const Footer = () => (
+  <div data-testid="footer" className="footer">
+    <Link to="/bebidas">
+      <img
+        data-testid="drinks-bottom-btn"
+        src={drinkIcon}
+        alt="drinkIcon"
+      />
+    </Link>
+    <Link to="/explorar">
+      <img
+        data-testid="profile-top-btn"
+        src={exploreIcon}
+        alt="exploreIcon"
+      />
+    </Link>
+    <Link to="/comidas">
+      <img
+        data-testid="food-bottom-btn"
+        src={mealIcon}
+        alt="mealIcon"
+      />
+    </Link>
+  </div>
+);
 
 export default Footer;
