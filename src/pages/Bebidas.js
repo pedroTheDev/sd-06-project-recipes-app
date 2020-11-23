96369814c8d42236ce479060aa17ab7576865097
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-11-23 16:24:30 -0300

    Implementa buscas na API de comidas e bebidas

--- src/pages/Bebidas.js
+++ src/pages/Bebidas.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 import Header from '../components/Header';
 
-function Bebidas() {
+function Bebidas({ retrievedDrinks }) {
+  const checkRequestSize = (request) => {
+    if (request === null) {
+      alert('Nenhuma receita encontrada');
+    } else if (request.length === 0) {
+      return null;
+    } else if (request.length === 1) {
+      const { idDrink } = request[0];
+      return <Redirect to={ `/bebidas/${idDrink}` } />;
+    } else {
+      return request.map((drink) => (
+        <div key={ drink.idDrink }>{ drink.strDrink }</div>
+      ));
+    }
+    return null;
+  };
   return (
     <main>
       <Header pageName="Bebidas" renderSearch />
       Bebidas Page
+      { checkRequestSize(retrievedDrinks) }
     </main>
   );
 }
 
-export default Bebidas;
+const mapStateToProps = (state) => ({
+  retrievedDrinks: state.searchReducer.drinks,
+});
+
+Bebidas.propTypes = {
+  retrievedDrinks: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps)(Bebidas);
--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 import Header from '../components/Header';
 
-function Comidas() {
+function Comidas({ retrievedFood }) {
+  const checkRequestSize = (request) => {
+    if (request === null) {
+      alert('Nenhuma receita encontrada');
+    } else if (request.length === 0) {
+      return null;
+    } else if (request.length === 1) {
+      const { idMeal } = request[0];
+      return <Redirect to={ `/comidas/${idMeal}` } />;
+    } else {
+      return request.map((meal) => (
+        <div key={ meal.idMeal }>{ meal.strMeal }</div>
+      ));
+    }
+    return null;
+  };
   return (
     <main>
       <Header pageName="Comidas" renderSearch />
       Comidas Page
+      { checkRequestSize(retrievedFood) }
+
     </main>
   );
 }
 
-export default Comidas;
+const mapStateToProps = (state) => ({
+  retrievedFood: state.searchReducer.meals,
+});
+
+Comidas.propTypes = {
+  retrievedFood: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps)(Comidas);
