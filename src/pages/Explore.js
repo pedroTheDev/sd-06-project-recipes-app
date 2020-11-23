2028fcf6e62ea4fbbaf769294fce22499b7cdc93
Danilo <dansantosalfa@gmail.com>
2020-11-23 16:54:49 -0300

    Lint

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -7,15 +7,15 @@ function Explore() {
     <div>
       <h1>Explore</h1>
       <Link to="/explorar/comidas">
-          <button type="button" data-testid="explore-food">
-            Explorar Comidas
-          </button>
-        </Link>
-        <Link to="/explorar/bebidas">
-          <button type="button" data-testid="explore-drinks">
-            Explorar Bebidas
-          </button>
-        </Link>
+        <button type="button" data-testid="explore-food">
+          Explorar Comidas
+        </button>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <button type="button" data-testid="explore-drinks">
+          Explorar Bebidas
+        </button>
+      </Link>
       <Footer />
     </div>
   );
