8d8d33c76f8835814b41963a41a599de17730ba2
danieleperse <danieleperse3@gmail.com>
2020-11-23 17:51:28 -0300

    req. de explorar

--- src/components/ExploreDrinks.js
+++ src/components/ExploreDrinks.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Footer from '../components/Footer';
+import Footer from './Footer';
 import profileIcon from '../images/profileIcon.svg';
 
 function ExploreDrinks() {
-
   return (
     <div>
       <Link to="/perfil">
         <img src={ profileIcon } alt="Profile" data-testid="profile-top-btn" />
       </Link>
       <h2 data-testid="page-title">Explorar Bebidas</h2>
-      <Link>
+      <Link to="/explorar/bebidas/ingredientes">
         <button
           data-testid="explore-by-ingredient"
+          type="button"
         >
           Por Ingredientes
         </button>
       </Link>
-      <Link>
-        <button
-          data-testid="explore-surprise"
-        >
-          Me Surpreenda!
-        </button>
-      </Link>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
--- src/components/ExploreFoods.js
+++ src/components/ExploreFoods.js
@@ -4,34 +4,34 @@ import Footer from './Footer';
 import profileIcon from '../images/profileIcon.svg';
 
 function ExploreFoods() {
-
   return (
     <div>
       <Link to="/perfil">
         <img src={ profileIcon } alt="Profile" data-testid="profile-top-btn" />
       </Link>
       <h2 data-testid="page-title">Explorar Comidas</h2>
-      <Link>
+      <Link to="/explorar/comidas/ingredientes">
         <button
           data-testid="explore-by-ingredient"
+          type="button"
         >
           Por Ingredientes
         </button>
       </Link>
-      <Link>
+      <Link to="/explorar/comidas/area">
         <button
           data-testid="explore-by-area"
+          type="button"
         >
           Por Local de Origem
         </button>
       </Link>
-      <Link>
-        <button
-          data-testid="explore-surprise"
-        >
-          Me Surpreenda!
-        </button>
-      </Link>
+      <button
+        data-testid="explore-surprise"
+        type="button"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
