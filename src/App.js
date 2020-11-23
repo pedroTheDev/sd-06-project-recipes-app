eb50b980c6b0ed1da53a499a0e5aa96d85320bb9
isabellavjs <isabellavjs@gmail.com>
2020-11-23 20:04:49 -0300

    Adiciona testes para o Header

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 import Login from './pages/Login/Login';
 import MealsPage from './pages/MealsPage/MealsPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+import RecipesAppProvider from './context/RecipesAppProvider';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/comidas" component={ MealsPage } />
-      <Route exact path="/perfil" component={ ProfilePage } />
-    </Switch>
+    <RecipesAppProvider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/comidas" component={ MealsPage } />
+        <Route exact path="/perfil" component={ ProfilePage } />
+      </Switch>
+    </RecipesAppProvider>
   );
 }
 
