290bcd4003a46701a2c7eb339acc052c3651c5bf
Lais Gurgel <laisgurgelf@gmail.com>
2020-11-23 14:55:41 -0300

    Requirement 67, 68 and 69

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import AppContext from '../context/AppContext';
@@ -12,6 +13,12 @@ function Explore() {
   return (
     <div>
       <Header />
+      <Link to="/explorar/comidas">
+        <button data-testid="explore-food" type="button">Explorar Comidas</button>
+      </Link>
+      <Link to="explorar/bebidas">
+        <button data-testid="explore-drinks" type="button">Explorar Bebidas</button>
+      </Link>
       <Footer />
     </div>
   );
