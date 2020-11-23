ab70ee92a289aa6e6ee3cc78ee0ec4a2b2e3eab1
Ana Higo <anahigo.arq@gmail.com>
2020-11-23 17:30:40 -0300

    Requisito 70, 71, 72, 73 - Ok

--- src/pages/ExplorarBebidas.js
+++ src/pages/ExplorarBebidas.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Footer } from '../components';
+import { Link } from 'react-router-dom';
+import { Header, Footer } from '../components';
 
 function ExplorarBebidas() {
   return (
     <div className="food">
-      <span>Aqui é para explorar bebidas...</span>
+      <Header />
+      <div className="div-buttons">
+        <Link to="/explorar/bebidas/ingredientes">
+          <button
+            className="button-ingredient"
+            data-testid="explore-by-ingredient"
+            type="button"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <br />
+        <button
+          className="button-surprise"
+          data-testid="explore-surprise"
+          type="button"
+        >
+          Me Surpreenda!
+        </button>
+      </div>
       <Footer />
     </div>
   );
--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,10 +1,40 @@
 import React from 'react';
-import { Footer } from '../components';
+import { Link } from 'react-router-dom';
+import { Header, Footer } from '../components';
 
 function ExplorarComidas() {
   return (
     <div className="food">
-      <span>Aqui é para explorar comidas...</span>
+      <Header />
+      <div className="div-buttons">
+        <Link to="/explorar/comidas/ingredientes">
+          <button
+            className="button-ingredient"
+            data-testid="explore-by-ingredient"
+            type="button"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <br />
+        <Link to="/explorar/comidas/area">
+          <button
+            className="button-area"
+            data-testid="explore-by-area"
+            type="button"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+        <br />
+        <button
+          className="button-surprise"
+          data-testid="explore-surprise"
+          type="button"
+        >
+          Me Surpreenda!
+        </button>
+      </div>
       <Footer />
     </div>
   );
