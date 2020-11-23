63ab860d4b7a9d1d7868b010d7b64a89ba18e243
Matheuscara <matheuscara2000@gmail.com>
2020-11-23 20:14:26 -0300

    Iniciando

--- src/Pages/Comidas.jsx
+++ src/Pages/Comidas.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Header from '../components/Header';
+import ResultsAPI from '../components/ResultsAPI';
 import Footer from '../components/Footer';
 
 const Comidas = () => (
   <div>
     <Header />
+    <ResultsAPI />
     <Footer />
   </div>
 );
--- src/services/aPI.js
+++ src/services/aPI.js
@@ -1,52 +1,42 @@
 export async function searchDrinkIngredients(ingredient) {
-  const URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=`
+  const URL = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
 
-  const response = await fetch(`${URL}${ingredient}`)
+  const response = await fetch(`${URL}${ingredient}`);
   const json = response.json();
   console.log(json);
   return json;
 }
 
 export async function searchDrinkName(name) {
-  const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`
+  const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-  const response = await fetch(`${URL}${name}`)
+  const response = await fetch(`${URL}${name}`);
   const json = response.json();
   console.log(json);
   return json;
 }
 
 export async function searchDrinkFirstLetter(letter) {
-  const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=`
+  const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
 
-  const response = await fetch(`${URL}${letter}`)
+  const response = await fetch(`${URL}${letter}`);
   const json = response.json();
-  console.log(json);
   return json;
 }
 
 export async function searchFoodIngredients(ingredient) {
-  const URL = `https://www.themealdb.com/api/json/v1/1/filter.php?i=`
+  const URL = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
 
-  const response = await fetch(`${URL}${ingredient}`)
+  const response = await fetch(`${URL}${ingredient}`);
   const json = response.json();
   console.log(json);
   return json;
 }
 
 export async function searchFoodName(name) {
-  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=`
+  const URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
-  const response = await fetch(`${URL}${name}`)
-  const json = response.json();
-  console.log(json);
-  return json;
-}
-
-export async function searchDrinkFirstLetter(letter) {
-  const URL = `https://www.themealdb.com/api/json/v1/1/search.php?f=`
-
-  const response = await fetch(`${URL}${letter}`)
+  const response = await fetch(`${URL}${name}`);
   const json = response.json();
   console.log(json);
   return json;
