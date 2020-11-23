1b63231923787c49bd236ca4017a6054dbe4ea48
Kesley Lahoz <namielahoz@gmail.com>
2020-11-23 11:02:16 -0300

    pull da main

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import MealsContext from '../context/MealsContext';
 
 function Perfil() {
+  const [email, setEmail] = useState()
+  const {
+    email,
+    setEmail,
+  } = useContext(MealsContext);
+  console.log(email)
+
   return (
     <div>
       <h1 data-testid="page-title">Perfil</h1>
+      <div data-testid="profile-email">
+        aqui vai ficar o email
+      </div>
+      <button 
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Receitas Feitas
+      </button>
+      <button 
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Receitas Favoritas
+      </button>
     </div>
   );
 }
