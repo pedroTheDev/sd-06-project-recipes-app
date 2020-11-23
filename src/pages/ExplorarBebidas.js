fbb342dcf87189edac4892c24cc3bd6d3ab3bda4
LeandroParisi <lepcbelisario@gmail.com>
2020-11-23 18:36:13 -0300

    Correcting EsLint 3

--- src/pages/ExplorarBebidas.js
+++ src/pages/ExplorarBebidas.js
@@ -1,9 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import Header from '../components/Header';
-=======
 import Footer from '../components/Footer';
->>>>>>> bf46f8fd81803886ed191c8195cc70ddfb300773
 
 function ExplorarBebidas() {
   return (
