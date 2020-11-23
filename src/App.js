fd444467242e3468b0cefa2a96147cb78399a110
Bruna Franciele <brunafranciele1902@gmail.com>
2020-11-23 17:17:56 -0300

    atualização de rotas

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Login from './pages/Login/Login';
 import RecipesDetails from './pages/RecipesDetails/RecipesDetails';
 import RecipesPage from './pages/RecipesPage/RecipesPage';
-
++
 function App() {
   return (
     <Switch>
