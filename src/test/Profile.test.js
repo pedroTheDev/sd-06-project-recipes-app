2caada795dbc5e3e963f05004e9ba01902c49d54
Lais Gurgel <laisgurgelf@gmail.com>
2020-11-23 19:18:19 -0300

    Requirement 82-87: Profile test

--- src/test/Profile.test.js
+++ src/test/Profile.test.js
@@ -3,10 +3,19 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import Profile from '../pages/Profile';
+import AppProvider from '../provider/AppProvider';
 
 describe('A página de perfil', () => {
+  beforeAll(() => {
+    const email = 'email@email.com';
+    localStorage.user = JSON.stringify({ email });
+  });
   it('Possui três botões: Receitas Favoritas, Receitas Feitas e Sair', () => {
-    renderWithRouter(<Profile />);
+    renderWithRouter(
+      <AppProvider>
+        <Profile />
+      </AppProvider>,
+    );
     const doneBtn = screen.queryByTestId('profile-done-btn');
     const favoriteBtn = screen.queryByTestId('profile-favorite-btn');
     const logoutBtn = screen.queryByTestId('profile-logout-btn');
@@ -15,21 +24,33 @@ describe('A página de perfil', () => {
     expect(logoutBtn).toBeInTheDocument();
   });
   it('O botão Receitas Favoritas encaminha para a página de mesmo nome', () => {
-    const { history } = renderWithRouter(<Profile />);
+    const { history } = renderWithRouter(
+      <AppProvider>
+        <Profile />
+      </AppProvider>,
+    );
     const button = screen.queryByTestId('profile-favorite-btn');
     userEvent.click(button);
     const { pathname } = history.location;
     expect(pathname).toBe('/receitas-favoritas');
   });
   it('O botão Receitas Feitas encaminha para a página de mesmo nome', () => {
-    const { history } = renderWithRouter(<Profile />);
+    const { history } = renderWithRouter(
+      <AppProvider>
+        <Profile />
+      </AppProvider>,
+    );
     const button = screen.queryByTestId('profile-done-btn');
     userEvent.click(button);
     const { pathname } = history.location;
     expect(pathname).toBe('/receitas-feitas');
   });
   it('O botão de Log Out encaminha para a página de login', () => {
-    const { history } = renderWithRouter(<Profile />);
+    const { history } = renderWithRouter(
+      <AppProvider>
+        <Profile />
+      </AppProvider>,
+    );
     const button = screen.queryByTestId('profile-logout-btn');
     userEvent.click(button);
     const { pathname } = history.location;
