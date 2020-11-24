2ffc48f49d55a3802d2f996818ba7fdd0bed6f20
Paulo <paulocheloni@gmail.com>
2020-11-24 15:11:02 -0300

    fixing merge  conflits

---
+++ src/pages/Food.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+import RecipeResults from '../components/RecipeResults';
+import findMatchInKeys from '../helpers/assets';
+import Footer from '../components/Footer';
+
+function Food(props) {
+  const { history: { location: { pathname } }, recipes } = props;
+  const [showMultipleResults, setShowMultipleResults] = useState(false);
+
+  const renderRecipesResults = () => {
+    const maxRecipesNumber = 12;
+    if (showMultipleResults) {
+      return (
+        recipes.results.filter((_recipe, index) => index < maxRecipesNumber)
+          .map((recipe, index) => (<RecipeResults
+            pathname={ pathname }
+            recipe={ recipe }
+            key={ recipe[findMatchInKeys('id', recipe)] }
+            recipeIndex={ index }
+          />))
+      );
+    }
+    return null;
+  };
+
+  return (
+    <>
+      <Header
+        pathname={ pathname }
+        setShowMultipleResults={ setShowMultipleResults }
+      />
+      {renderRecipesResults()}
+      <Footer />
+    </>
+  );
+}
+
+const mapStateToProps = (state) => ({
+  recipes: state.searchRecipes.recipes,
+});
+
+export default connect(mapStateToProps, null)(Food);
+
+Food.propTypes = {
+  recipes: PropTypes.shape({
+    type: PropTypes.string,
+    results: PropTypes.arrayOf(PropTypes.any),
+  }).isRequired,
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      pathname: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
