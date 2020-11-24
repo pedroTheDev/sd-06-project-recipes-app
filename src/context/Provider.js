4b36364d56621dd5c71ee5284311734dcaad850a
FlavioSugano <flavio.sugano@gmail.com>
2020-11-24 18:31:47 -0300

    Requisito 25 e 26, tela principal de Comidas

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
+import fetchMeal from '../services/fetchMeal';
 
 function RecipesAppProvider({ children }) {
-  const [estadoTeste, setEstadoTeste] = useState('teste');
+  const [meals, setMeals] = useState([]);
+
+  const mealsToRender = async () => {
+    let fetchedMeals = await fetchMeal('name');
+    const first = 0;
+    const twelfth = 12;
+
+    fetchedMeals = fetchedMeals.slice(first, twelfth);
+    setMeals(fetchedMeals);
+  };
 
   const contextValue = {
-    estadoTeste,
-    setEstadoTeste,
+    meals,
+    mealsToRender,
   };
 
   return (
--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Card from '../components/RecipeCard';
+import Card from '../components/MealRecipeCard';
+import Context from '../context/Context';
 
 function Meals() {
   const TRUE = true;
+  const { meals, mealsToRender } = useContext(Context);
+
+  useEffect(() => {
+    mealsToRender();
+  }, []);
+
   return (
     <div>
       <Header title="Comidas" search={ TRUE } />
-      <Card />
+      <div className="recipes-cards">
+        {!meals ? 'Loading...' : meals.map((meal, index) => (
+          <Card key={ meal.strMeal } mealInfo={ meal } index={ index } />
+        ))}
+      </div>
       <Footer />
     </div>
   );
