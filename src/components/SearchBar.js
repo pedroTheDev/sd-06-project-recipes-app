73cf93fa58ba59365df27371ffb1eb79b2b2f5b0
CristianoNunes <cristianonunes99@gmail.com>
2020-11-24 17:55:03 -0300

    Atualizando CSS do Header

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,39 +2,54 @@ import React from 'react';
 
 export default function SearchBar() {
   return (
-    <div className="searchBar">
-      <input data-testid="search-input" type="text" />
-      <label htmlFor="ingredient">
-        <input
-          data-testid="ingredient-search-radio"
-          type="radio"
-          id="ingredient"
-          name="ingredient"
-          value="ingredient"
-        />
-        Ingrediente
-      </label>
-      <label htmlFor="name">
-        <input
-          data-testid="name-search-radio"
-          type="radio"
-          id="name"
-          name="name"
-          value="name"
-        />
-        Nome
-      </label>
-      <label htmlFor="firstLetter">
-        <input
-          data-testid="first-letter-search-radio"
-          type="radio"
-          id="firstLetter"
-          name="firstLetter"
-          value="firstLetter"
-        />
-        Primeira Letra
-      </label>
-      <button data-testid="exec-search-btn" type="button">Buscar</button>
+    <div className="form-group searchBar">
+      <div className="col-sm-8">
+        <input className="form-control" data-testid="search-input" type="text" />
+      </div>
+      <div className="form-check radioBtns">
+        <label className="form-check-label" htmlFor="ingredient">
+          <input
+            data-testid="ingredient-search-radio"
+            className="form-check-input"
+            type="radio"
+            id="ingredient"
+            name="filter-info"
+            value="ingredient"
+          />
+          Ingrediente
+        </label>
+        <label className="form-check-label" htmlFor="name">
+          <input
+            data-testid="name-search-radio"
+            className="form-check-input"
+            type="radio"
+            id="name"
+            name="filter-info"
+            value="name"
+          />
+          Nome
+        </label>
+        <label className="form-check-label" htmlFor="firstLetter">
+          <input
+            data-testid="first-letter-search-radio"
+            className="form-check-input"
+            type="radio"
+            id="firstLetter"
+            name="filter-info"
+            value="firstLetter"
+          />
+          Primeira Letra
+        </label>
+      </div>
+      <div>
+        <button
+          data-testid="exec-search-btn"
+          className="btn btn-primary"
+          type="button"
+        >
+          Buscar
+        </button>
+      </div>
     </div>
   );
 }
