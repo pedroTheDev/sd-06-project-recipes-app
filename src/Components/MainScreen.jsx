c75d1c69f89e46b2c639f3dc86caa523c52de656
luiz <phellipe501@gmail.com>
2020-11-24 15:58:46 -0300

    Filters req 27

--- src/Components/MainScreen.jsx
+++ src/Components/MainScreen.jsx
@@ -1,14 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import RecipeContext from '../hooks/RecipeContext';
+import recipeRequest from '../services/recipeRequest';
 
 export default function MainScreen() {
   const history = useHistory();
-  const { foodRecipes, drinkRecipes, isLoading } = useContext(RecipeContext);
+  const { foodRecipes, drinkRecipes, isLoading, foodFilter, drinkFilter } = useContext(RecipeContext);
   const doze = 12;
   const { pathname } = history.location;
+  console.log(foodFilter, drinkFilter);
+  // const renderFilters = () => {
+  // if (pathname === '/comidas') {
+  // }
+  // };
+
   const renderCards = () => {
-    console.log('texto', drinkRecipes);
     if (pathname === '/comidas') {
       return foodRecipes.filter((food, index) => index < doze)
         .map((food, index) => (
--- src/hooks/Provider.js
+++ src/hooks/Provider.js
@@ -7,13 +7,19 @@ const Provider = ({ children }) => {
   const [foodRecipes, setFoodRecipes] = useState([]);
   const [drinkRecipes, setDrinkRecipes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [foodFilter, setFoodFilter] = useState([]);
+  const [drinkFilter, setDrinkFilter] = useState([]);
 
   const getAPI = async () => {
     const foodRequest = await recipeRequest('https://www.themealdb.com/api/json/v1/1/search.php?s=A');
     const drinkRequest = await recipeRequest('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=A');
+    const foodFilterRequest = await recipeRequest('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+    const drinkFilterRequest = await recipeRequest('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
     setIsLoading(true);
     await setDrinkRecipes(drinkRequest.drinks);
     await setFoodRecipes(foodRequest.meals);
+    await setFoodFilter(foodFilterRequest.meals);
+    await setDrinkFilter(drinkFilterRequest.drinks);
     console.log('texto bebidas', drinkRecipes);
     setIsLoading(false);
   };
@@ -27,6 +33,8 @@ const Provider = ({ children }) => {
     drinkRecipes,
     isLoading,
     setIsLoading,
+    foodFilter,
+    drinkFilter,
   };
 
   return (
