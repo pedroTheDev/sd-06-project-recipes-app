8947a9ead987824d6f980958bb48359ea2ee79a5
Marxneves <marxkhristie@gmail.com>
2020-11-24 20:53:28 -0300

    Requisitos 25 a 31 finalizados, faltando 32

--- src/components/RecipesCards.js
+++ src/components/RecipesCards.js
@@ -1,11 +1,48 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import RecipesContext from '../context/RecipesContext';
 import '../style/RecipeCards.css';
 
 function RecipesCards({ title }) {
-  const { data, isLoading } = useContext(RecipesContext);
+  const { data, setData, isLoading } = useContext(RecipesContext);
+  const [categories, setCategories] = useState('');
+  const [selectedCategorie, setSelectedCategorie] = useState('');
   const DOZE = 12;
+  const CINCO = 5;
+
+  useEffect(() => {
+    async function fetchCategories() {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+      const responseJson = await response.json();
+
+      const response2 = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+      const responseJson2 = await response2.json();
+      setCategories([responseJson, responseJson2]);
+    }
+    fetchCategories();
+  }, []);
+
+  const selectCategorie = async ({ innerText }) => {
+    if (innerText === 'All' || innerText === selectedCategorie) {
+      const responseMeal = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+      const responseMealJson = await responseMeal.json();
+
+      const responseDrinks = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+      const responseDrinksJson = await responseDrinks.json();
+      return setData([responseMealJson, responseDrinksJson]);
+    }
+    if (title === 'Comidas') {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${innerText}`);
+      const responseJson = await response.json();
+      setSelectedCategorie(innerText);
+      return setData([responseJson, data[1]]);
+    }
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${innerText}`);
+    const responseJson = await response.json();
+    setSelectedCategorie(innerText);
+    return setData([data[0], responseJson]);
+  };
+
   if (isLoading) {
     return <h2>Loading...</h2>;
   }
@@ -13,6 +50,22 @@ function RecipesCards({ title }) {
   if (title === 'Comidas') {
     return (
       <div>
+        <button
+          type="button"
+          onClick={ ({ target }) => selectCategorie(target) }
+        >
+          All
+        </button>
+        {categories[0].meals.filter((_, index) => index < CINCO)
+          .map(({ strCategory }) => (
+            <button
+              key={ strCategory }
+              data-testid={ `${strCategory}-category-filter` }
+              type="button"
+              onClick={ ({ target }) => selectCategorie(target) }
+            >
+              { strCategory }
+            </button>)) }
         {
           data[0].meals.filter((_, index) => index < DOZE)
             .map(({ idMeal, strMeal, strMealThumb }, index) => (
@@ -35,6 +88,22 @@ function RecipesCards({ title }) {
   }
   return (
     <div>
+      <button
+        type="button"
+        onClick={ ({ target }) => selectCategorie(target) }
+      >
+        All
+      </button>
+      {categories[1].drinks.filter((_, index) => index < CINCO)
+        .map(({ strCategory }) => (
+          <button
+            data-testid={ `${strCategory}-category-filter` }
+            key={ strCategory }
+            type="button"
+            onClick={ ({ target }) => selectCategorie(target) }
+          >
+            { strCategory }
+          </button>)) }
       {
         data[1].drinks.filter((_, index) => index < DOZE)
           .map(({ idDrink, strDrink, strDrinkThumb }, index) => (
