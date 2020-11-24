70c0cc22eb063f98b1de874c8283c05ffdbf308d
Marxneves <marxkhristie@gmail.com>
2020-11-24 19:38:11 -0300

    Main-group-3: Refatorando funcoes e corrigindo requisitos 10 e 11

--- src/components/RecipesCards.js
+++ src/components/RecipesCards.js
@@ -1,38 +1,63 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import RecipesContext from '../context/RecipesContext';
 import '../style/RecipeCards.css';
 
-function RecipesCards({ title, index, recipe }) {
+function RecipesCards({ title }) {
+  const { data, isLoading } = useContext(RecipesContext);
+  const DOZE = 12;
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   if (title === 'Comidas') {
-    const { strMeal, strMealThumb } = recipe;
     return (
-      <div
-        data-testid={ `${index}-recipe-card` }
-        className="card-recipe"
-      >
-        <img data-testid={ `${index}-card-img` } src={ strMealThumb } alt={ strMeal } />
-        <h1 data-testid={ `${index}-card-name` }>{ strMeal }</h1>
+      <div>
+        {
+          data[0].meals.filter((_, index) => index < DOZE)
+            .map(({ idMeal, strMeal, strMealThumb }, index) => (
+              <div
+                key={ idMeal }
+                data-testid={ `${index}-recipe-card` }
+                className="card-recipe"
+              >
+                <img
+                  data-testid={ `${index}-card-img` }
+                  src={ strMealThumb }
+                  alt={ strMeal }
+                />
+                <h1 data-testid={ `${index}-card-name` }>{ strMeal }</h1>
+              </div>
+            ))
+        }
       </div>
     );
   }
-  const { strDrink, strDrinkThumb } = recipe;
   return (
-    <div data-testid={ `${index}-recipe-card` } className="card-recipe">
-      <img data-testid={ `${index}-card-img` } src={ strDrinkThumb } alt={ strDrink } />
-      <h1 data-testid={ `${index}-card-name` }>{ strDrink }</h1>
+    <div>
+      {
+        data[1].drinks.filter((_, index) => index < DOZE)
+          .map(({ idDrink, strDrink, strDrinkThumb }, index) => (
+            <div
+              key={ idDrink }
+              data-testid={ `${index}-recipe-card` }
+              className="card-recipe"
+            >
+              <img
+                data-testid={ `${index}-card-img` }
+                src={ strDrinkThumb }
+                alt={ strDrink }
+              />
+              <h1 data-testid={ `${index}-card-name` }>{ strDrink }</h1>
+            </div>
+          ))
+      }
     </div>
   );
 }
 
 RecipesCards.propTypes = {
   title: PropTypes.string.isRequired,
-  index: PropTypes.number.isRequired,
-  recipe: PropTypes.shape({
-    strMeal: PropTypes.string,
-    strMealThumb: PropTypes.string,
-    strDrink: PropTypes.string,
-    strDrinkThumb: PropTypes.string,
-  }).isRequired,
 };
 
 export default RecipesCards;
