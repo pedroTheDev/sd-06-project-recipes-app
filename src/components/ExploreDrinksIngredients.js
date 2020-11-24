d1df3827ba0c5d875430986773737a16969830c3
danieleperse <danieleperse3@gmail.com>
2020-11-24 20:19:58 -0300

    requisito 75 ok

--- src/components/ExploreDrinksIngredients.js
+++ src/components/ExploreDrinksIngredients.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function ExploreDrinksIngredients() {
+  const [data, setData] = useState([]);
+  const MAX_NUMBER_OF_CARDS = 12;
+
+  const apiIngredients = async () => {
+    const apiRequest = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
+    const response = await apiRequest.json();
+    setData(response.drinks)
+  }
+
+  useEffect(() => {
+    apiIngredients();
+  }, []);
+
   return (
-    <div>explore foods ingredients</div>
-    // <div data-testid="${index}-ingredient-card">
-    //   <img data-testid="${index}-card-img"/>
-    //   <h3 data-testid="${index}-card-name"></h3>
-    // </div>
+    data.map((drinks, index) => (
+      <div key={index} data-testid={ `${index}-ingredient-card` }>
+        <img
+          data-testid={ `${index}-card-img` }
+          width="60px"
+          src={`https://www.themealdb.com/images/ingredients/${drinks.strIngredient1}.png`}
+        />
+        <h3
+          data-testid={ `${index}-card-name` }
+        >{ drinks.strIngredient1 }</h3>
+      </div>
+    )).filter((_, index) => index < MAX_NUMBER_OF_CARDS)
   );
 }
 
--- src/components/ExploreFoodsIngredients.js
+++ src/components/ExploreFoodsIngredients.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function ExploreFoodsIngredients() {
+  const [data, setData] = useState([]);
+  const MAX_NUMBER_OF_CARDS = 12;
+
+  const apiIngredients = async () => {
+    const apiRequest = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+    const response = await apiRequest.json();
+    setData(response.meals)
+  }
+
+  useEffect(() => {
+    apiIngredients();
+  }, []);
+
   return (
-    <div>explore foods ingredients</div>
-    // <div data-testid="${index}-ingredient-card">
-    //   <img data-testid="${index}-card-img"/>
-    //   <h3 data-testid="${index}-card-name"></h3>
-    // </div>
+    data.map((meals, index) => (
+      <div key={index} data-testid={ `${index}-ingredient-card` }>
+        <img
+          data-testid={ `${index}-card-img` }
+          width="60px"
+          src={`https://www.themealdb.com/images/ingredients/${meals.strIngredient}.png`}
+        />
+        <h3
+          data-testid={ `${index}-card-name` }
+        >{ meals.strIngredient }</h3>
+      </div>
+    )).filter((_, index) => index < MAX_NUMBER_OF_CARDS)
   );
 }
 
