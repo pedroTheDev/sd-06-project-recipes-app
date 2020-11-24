4746b8a62b6b89a5c0e8e1bd5c53546bccde716d
Isadora Koga <isadorakoga710@gmail.com>
2020-11-24 16:57:00 -0300

    :memo: Function render filters

--- src/Components/MainScreen.jsx
+++ src/Components/MainScreen.jsx
@@ -1,22 +1,47 @@
 import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import RecipeContext from '../hooks/RecipeContext';
-import recipeRequest from '../services/recipeRequest';
 
 export default function MainScreen() {
   const history = useHistory();
   const { foodRecipes, drinkRecipes, isLoading, foodFilter, drinkFilter } = useContext(RecipeContext);
-  const doze = 12;
+  const twelve = 12;
+  const five = 5;
   const { pathname } = history.location;
-  console.log(foodFilter, drinkFilter);
-  // const renderFilters = () => {
-  // if (pathname === '/comidas') {
-  // }
-  // };
+  
+  const renderFilters = () => {
+    if (pathname === '/comidas') {
+      console.log('entrei no filtro comidas');
+      return foodFilter.filter((_, index) => index < five)
+          .map((filter, index) => (
+            <button
+              type="button"
+              data-testid={ `${filter.strCategory}-category-filter` }
+              key={index}
+              // onClick={ }
+            >
+              {filter.strCategory}
+            </button>
+          ));
+    }
+    if (pathname === '/bebidas') {
+      console.log('entrei no filtro bebidas');
+      return drinkFilter.filter((_, index) => index < five)
+          .map((filter, index) => (
+            <button
+              type="button"
+              data-testid={ `${filter.strCategory}-category-filter` }
+              key={index}
+            >
+              {filter.strCategory}
+            </button>
+          ));
+    }
+  };
 
   const renderCards = () => {
     if (pathname === '/comidas') {
-      return foodRecipes.filter((food, index) => index < doze)
+      return foodRecipes.filter((food, index) => index < twelve)
         .map((food, index) => (
           <div data-testid={ `${index}-recipe-card` } key="index">
             <img
@@ -31,7 +56,7 @@ export default function MainScreen() {
 
     if (pathname === '/bebidas') {
       return (drinkRecipes && drinkRecipes.length && drinkRecipes
-        .filter((drinks, index) => index < doze)
+        .filter((drink, index) => index < twelve)
         .map((drinks, index) => (
           <div data-testid={ `${index}-recipe-card` } key="index">
             <img
@@ -44,5 +69,15 @@ export default function MainScreen() {
         )));
     }
   };
-  return <div>{ isLoading ? <p>Loading...</p> : renderCards() }</div>;
+
+  return (
+    <div>
+      <div className="filter-container">
+        <button type="button">All</button>
+        {renderFilters()}
+      </div>
+      <div className="recipes-container">
+        { isLoading ? <p>Loading...</p> : renderCards() }
+      </div>
+    </div>);
 }
