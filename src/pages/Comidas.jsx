6680c12552fe59bbbacb0d8cef3fc3de099e9783
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-11-24 15:59:29 -0300

    group 19 resolvendo conflitos de merge

--- src/pages/Comidas.jsx
+++ src/pages/Comidas.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import RecipesAppContext from '../hooks/RecipesAppContext';
+import Footer from '../components/Footer';
 
 function Comidas({ history }) {
   const {
@@ -23,28 +24,31 @@ function Comidas({ history }) {
   return (
     <div>
       <Header name="Comidas" button={ buttonIs } />
-      {cardFood.slice(ofTheFirstParameter, upToParameter12).map(({
-        idMeal,
-        strMeal,
-        strMealThumb,
-      }, index) => (
-        <Link
-          key={ idMeal }
-          to={ `/comidas/${idMeal}` }
-          data-testid={ `${index}-recipe-card` }
-        >
-          <img
-            src={ strMealThumb }
-            alt={ strMeal }
-            data-testid={ `${index}-card-img` }
-          />
-          <h4
-            data-testid={ `${index}-card-name` }
+      <div>
+        {cardFood.slice(ofTheFirstParameter, upToParameter12).map(({
+          idMeal,
+          strMeal,
+          strMealThumb,
+        }, index) => (
+          <Link
+            key={ idMeal }
+            to={ `/comidas/${idMeal}` }
+            data-testid={ `${index}-recipe-card` }
           >
-            { strMeal }
-          </h4>
-        </Link>
-      ))}
+            <img
+              src={ strMealThumb }
+              alt={ strMeal }
+              data-testid={ `${index}-card-img` }
+            />
+            <h4
+              data-testid={ `${index}-card-name` }
+            >
+              { strMeal }
+            </h4>
+          </Link>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 }
