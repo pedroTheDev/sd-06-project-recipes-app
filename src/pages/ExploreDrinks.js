9b5af0a095f2168a3d017e15478ee19b58b5d398
RenatoCostaDev <renatotrybe@gmail.com>
2020-11-25 17:51:43 -0300

    Requirement 70 to 74

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,17 +1,38 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import AppContext from '../context/AppContext';
+import fetchRecipes from '../services';
 
 function ExploreDrinks() {
   const { setHeader } = useContext(AppContext);
+  const [randomId, setRandomId] = useState('');
+
+  const getRandomId = async () => {
+    const recipe = await fetchRecipes('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+    const id = recipe.drinks[0].idDrink;
+    setRandomId(id);
+  };
+
   useEffect(() => {
     setHeader({ page: 'Explorar Bebidas', search: false });
+    getRandomId();
   }, []);
 
   return (
     <div>
       <Header />
+      <Link to="/explorar/bebidas/ingredientes">
+        <button data-testid="explore-by-ingredient" type="button">
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to={ `/bebidas/${randomId}` }>
+        <button data-testid="explore-surprise" type="button">
+          Me Surpreenda!
+        </button>
+      </Link>
       <Footer />
     </div>
   );
--- src/pages/ExploreFoods.js
+++ src/pages/ExploreFoods.js
@@ -1,17 +1,43 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import AppContext from '../context/AppContext';
+import fetchRecipes from '../services';
 
 function ExploreFoods() {
   const { setHeader } = useContext(AppContext);
+  const [randomId, setRandomId] = useState('');
+
+  const getRandomId = async () => {
+    const recipe = await fetchRecipes('https://www.themealdb.com/api/json/v1/1/random.php');
+    const id = recipe.meals[0].idMeal;
+    setRandomId(id);
+  };
+
   useEffect(() => {
     setHeader({ page: 'Explorar Comidas', search: false });
+    getRandomId();
   }, []);
 
   return (
     <div>
       <Header />
+      <Link to="/explorar/comidas/ingredientes">
+        <button data-testid="explore-by-ingredient" type="button">
+          Por Ingredientes
+        </button>
+      </Link>
+      <Link to="/explorar/comidas/area">
+        <button data-testid="explore-by-area" type="button">
+          Por Local de Origem
+        </button>
+      </Link>
+      <Link to={ `/comidas/${randomId}` }>
+        <button data-testid="explore-surprise" type="button">
+          Me Surpreenda!
+        </button>
+      </Link>
       <Footer />
     </div>
   );
