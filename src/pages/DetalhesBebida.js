88e7d87549bfa42cb540d4049df3832b45b7df29
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-11-25 20:31:07 -0300

    req 33 ao 36

--- src/pages/DetalhesBebida.js
+++ src/pages/DetalhesBebida.js
@@ -2,11 +2,12 @@ import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReceitasContext from '../context/ReceitasContext';
 import MealsCard from '../components/MealsCard';
+import Header from '../components/Header';
 import { foodAPI } from '../services/foodAPI';
 
 function DetalhesBebida(props) {
   const { meals, drinks, setMeals } = useContext(ReceitasContext);
-  const { params: { id } } = props.match;
+  const { match: { params: { id } } } = props;
   const seis = 6;
 
   useEffect(() => {
@@ -32,42 +33,52 @@ function DetalhesBebida(props) {
   return ((!meals.length)
     ? <div>carregando...</div>
     : (
-      drinks.filter((item) => item.idDrink === id)
-        .map((drink, index) => (
-          <div key={index}>
-            <img data-testid="recipe-photo" src={drink.strDrinkThumb} alt="" />
-            <h2 data-testid="recipe-title" >{drink.strDrink}</h2>
-            <button data-testid="share-btn" type="button">Compartilhar</button>
-            <button data-testid="favorite-btn" type="button">Favoritar</button>
-            <p data-testid="recipe-category">{drink.strCategory}</p>
-            <ul>
-              {getIngredients(drink, /strIngredient/).map((item, index) => {
-                const measure = getIngredients(drink, /strMeasure/);
-                return (
-                  <li><p
-                    key={index}
-                    data-testid={`${index}-ingredient-name-and-measure`}
-                  >
-                    {`- ${item} - ${measure[index]} `}
-                  </p></li>
-                );
-              })}
-            </ul>
-            <h3>Instruções</h3>
-            <p data-testid="instructions">{drink.strInstructions}</p>
-            <button data-testid="start-recipe-btn" type="button">Iniciar Receita</button>
-            <h2>Receitas Recomendadas</h2>
-            <div>
-              {meals.length && (meals
-                .filter((_, index) => index < seis)
-                .map((food, i) => (
-                  <MealsCard key={food} food={food} index={i} />
-                )))}
-            </div>
-          </div>
-        ))
-    )
-  );
+      <section>
+        <Header title="Bebidas" />
+        {
+          drinks.filter((item) => item.idDrink === id)
+            .map((drink, index) => (
+              <div key={ index }>
+                <img data-testid="recipe-photo" src={ drink.strDrinkThumb } alt="" />
+                <h2 data-testid="recipe-title">{drink.strDrink}</h2>
+                <button data-testid="share-btn" type="button">Compartilhar</button>
+                <button data-testid="favorite-btn" type="button">Favoritar</button>
+                <p data-testid="recipe-category">{drink.strCategory}</p>
+                <ul>
+                  {getIngredients(drink, /strIngredient/).map((item, indx) => {
+                    const measure = getIngredients(drink, /strMeasure/);
+                    return (
+                      <li key={ indx }>
+                        <p
+                          data-testid={ `${indx}-ingredient-name-and-measure` }
+                        >
+                          { `- ${item} - ${measure[indx]} ` }
+                        </p>
+                      </li>
+                    );
+                  })}
+                </ul>
+                <h3>Instruções</h3>
+                <p data-testid="instructions">{drink.strInstructions}</p>
+                <button
+                  data-testid="start-recipe-btn"
+                  type="button"
+                >
+                  Iniciar Receita
+                </button>
+                <h2>Receitas Recomendadas</h2>
+                <div>
+                  {meals.length && (meals
+                    .filter((_, indx) => indx < seis)
+                    .map((food, i) => (
+                      <MealsCard key={ food } food={ food } index={ i } />
+                    )))}
+                </div>
+              </div>
+            ))
+        }
+      </section>
+    ));
 }
 
 DetalhesBebida.propTypes = {
--- src/pages/DetalhesComida.js
+++ src/pages/DetalhesComida.js
@@ -2,11 +2,12 @@ import React, { useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReceitasContext from '../context/ReceitasContext';
 import DrinksCard from '../components/DrinksCard';
+import Header from '../components/Header';
 import { drinkAPI } from '../services/drinkAPI';
 
 function DetalhesComida(props) {
   const { meals, drinks, setDrinks } = useContext(ReceitasContext);
-  const { params: { id } } = props.match;
+  const { match: { params: { id } } } = props;
   const seis = 6;
 
   useEffect(() => {
@@ -32,43 +33,55 @@ function DetalhesComida(props) {
   return ((!drinks.length)
     ? <div>carregando...</div>
     : (
-      meals.filter((item) => item.idMeal === id)
-        .map((meal, index) => (
-          <div key={index}>
-            <img data-testid="recipe-photo" src={meal.strMealThumb} alt="" />
-            <h2 data-testid="recipe-title" >{meal.strMeal}</h2>
-            <button data-testid="share-btn" type="button">Compartilhar</button>
-            <button data-testid="favorite-btn" type="button">Favoritar</button>
-            <p data-testid="recipe-category">{meal.strCategory}</p>
-            <ul>
-              {getIngredients(meal, /strIngredient/).map((item, index) => {
-                const measure = getIngredients(meal, /strMeasure/);
-                return (
-                  <li><p
-                    key={index}
-                    data-testid={`${index}-ingredient-name-and-measure`}
-                  >
-                    {`- ${item} - ${measure[index]} `}
-                  </p></li>
-                );
-              })}
-            </ul>
-            <h3>Instruções</h3>
-            <p data-testid="instructions">{meal.strInstructions}</p>
-            <iframe src={meal.strYoutube.replace('watch?v=', 'embed/')} />
-            <button data-testid="start-recipe-btn" type="button">Iniciar Receita</button>
-            <h2>Receitas Recomendadas</h2>
-            <div>
-              {drinks
-                .filter((x, index) => index < seis)
-                .map((drink, i) => (
-                  <DrinksCard key={drink} drink={drink} index={i} />
-                ))}
-            </div>
-          </div>
-        ))
-    )
-  );
+      <section>
+        <Header title="Comidas" />
+        {
+          meals.filter((item) => item.idMeal === id)
+            .map((meal, index) => (
+              <div key={ index }>
+                <img data-testid="recipe-photo" src={ meal.strMealThumb } alt="" />
+                <h2 data-testid="recipe-title">{meal.strMeal}</h2>
+                <button data-testid="share-btn" type="button">Compartilhar</button>
+                <button data-testid="favorite-btn" type="button">Favoritar</button>
+                <p data-testid="recipe-category">{meal.strCategory}</p>
+                <ul>
+                  {getIngredients(meal, /strIngredient/).map((item, indx) => {
+                    const measure = getIngredients(meal, /strMeasure/);
+                    return (
+                      <li key={ indx }>
+                        <p
+                          data-testid={ `${indx}-ingredient-name-and-measure` }
+                        >
+                          { `${item} - ${measure[indx]} ` }
+                        </p>
+                      </li>
+                    );
+                  })}
+                </ul>
+                <p data-testid="instructions">{meal.strInstructions}</p>
+                <iframe
+                  src={ meal.strYoutube.replace('watch?v=', 'embed/') }
+                  title="frame"
+                />
+                <button
+                  data-testid="start-recipe-btn"
+                  type="button"
+                >
+                  Iniciar Receita
+                </button>
+                <h2>Receitas Recomendadas</h2>
+                <div>
+                  {drinks
+                    .filter((_, indx) => indx < seis)
+                    .map((drink, i) => (
+                      <DrinksCard key={ drink } drink={ drink } index={ i } />
+                    ))}
+                </div>
+              </div>
+            ))
+        }
+      </section>
+    ));
 }
 
 DetalhesComida.propTypes = {
