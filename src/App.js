9d32d31002fda85e39fe16c6369b8bc7147f0254
Cyrano webdev <cyrano@cyranowebdev.com>
2020-11-25 16:39:30 -0300

    mudando arquivos de pastas e ajustando os index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Provider from './context/Provider';
-import './App.css';
 import Router from './router/Router';
 import history from './helpers/History';
+import './App.css';
 
 function App() {
   return (
