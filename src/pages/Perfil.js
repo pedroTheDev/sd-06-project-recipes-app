d95cbb97ae977804628a0d932b7f6ce8abfb1fd7
Daniel Jesus <dev.danieljs@gmail.com>
2020-11-24 21:13:42 -0300

    feat: renders email from localStorage

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,38 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-const Perfil = () => (
-  <section>
-    <Header title="Perfil" />
-    <div>
-      <span data-testid="profile-email">Email</span>
-    </div>
-    <div>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-      >
-        Receitas Feitas
-      </button>
+const Perfil = () => {
+  const [storageEmail, setStorageEmail] = useState('');
 
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-      >
-        Receitas Favoritas
-      </button>
+  const localStorageEmail = JSON.parse(localStorage.getItem('user'));
+  const email = Object.values(localStorageEmail);
 
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-      >
-        Sair
-      </button>
+  useEffect(() => {
+    setStorageEmail(email);
+  }, []);
 
-    </div>
-    <Footer />
-  </section>
-);
+  return (
+    <section>
+      <Header title="Perfil" />
+      <div>
+        <span data-testid="profile-email">{storageEmail}</span>
+      </div>
+      <div>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Sair
+        </button>
+
+      </div>
+      <Footer />
+    </section>
+  );
+};
 
 export default Perfil;
