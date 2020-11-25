3d8d929ab52832d0928a98d86b78f74cd4d3dd96
Daniel Jesus <dev.danieljs@gmail.com>
2020-11-24 21:09:27 -0300

    feat: implementing profile page

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -5,6 +5,32 @@ import Footer from '../components/Footer';
 const Perfil = () => (
   <section>
     <Header title="Perfil" />
+    <div>
+      <span data-testid="profile-email">Email</span>
+    </div>
+    <div>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Receitas Feitas
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Receitas Favoritas
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Sair
+      </button>
+
+    </div>
     <Footer />
   </section>
 );
