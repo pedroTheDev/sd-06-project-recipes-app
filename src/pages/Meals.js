d25b26d78404da7fca50e004644d9bd574c9ba3e
FlavioSugano <flavio.sugano@gmail.com>
2020-11-25 17:45:21 -0300

    Alteraçoes na logica dos Cards e no RecipEDetails

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import MealRecipeCard from '../components/MealRecipeCard';
+import MealRecipeCard from '../components/Cards';
 import Context from '../context/Context';
 
 function Meals() {
@@ -18,7 +18,7 @@ function Meals() {
       <Header title="Comidas" search={ TRUE } />
       <div className="recipes-cards">
         {recipes.map((meal, index) => (
-          <MealRecipeCard key={ meal.strMeal } mealInfo={ meal } index={ index } />
+          <MealRecipeCard key={ meal.strMeal } info={ meal } recipe="comidas" index={ index } />
         ))}
       </div>
       <Footer />
--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,12 +1,144 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import YouTube from 'react-youtube';
+import Context from '../context/Context';
+import Cards from '../components/Cards';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function RecipeDetails(props) {
-  const { match } = props;
-  const { id } = match.params;
+  const {
+    getMealDetail,
+    getDrinkDetail,
+    getRandomDrink,
+    getRandomMeal,
+    details,
+    random,
+  } = useContext(Context);
+  const { match: { path, params } } = props;
+  const [change, setChange] = useState(true);
+  const [fav, setFav] = useState('white');
+
+  useEffect(() => {
+    if (path === '/comidas/:id') {
+      getMealDetail(params.id);
+      getRandomDrink();
+    } else {
+      getDrinkDetail(params.id);
+      getRandomMeal();
+    }
+  }, [params.id]);
+
+  const getIngredients = (obj, filter) => {
+    const keys = [];
+    Object.keys(obj).forEach((key) => {
+      if (key && filter.test(key) && obj[key] !== '' && obj[key] !== null) {
+        keys.push(obj[key]);
+      }
+    });
+    return keys;
+  };
+
+  const getVideoId = (link) => link.split('=').pop();
 
   return (
-    <div>Página de Details</div>
+    <div>
+      <h1>Página de Details</h1>
+      {!details ? <p>LOADING...</p>
+        : details.map((recipe) => (
+          <div key={ recipe }>
+            <img
+              src={ path === '/comidas/:id' ? recipe.strMealThumb : recipe.strDrinkThumb }
+              alt="recipe_image"
+              data-testid="recipe-photo"
+            />
+            <h1 data-testid="recipe-title">
+              {path === '/comidas/:id' ? recipe.strMeal : recipe.strDrink}
+            </h1>
+            <input
+              type="image"
+              src={ shareIcon }
+              data-testid="share-btn"
+              alt="share-icon"
+            />
+            <input
+              type="image"
+              src={ fav === 'white' ? whiteHeartIcon : blackHeartIcon }
+              data-testid="favorite-btn"
+              alt={ fav === 'white' ? 'whiteHeartIcon' : 'blackHeartIcon' }
+              onClick={ () => (fav === 'white' ? setFav('black') : setFav('white')) }
+            />
+            <p data-testid="recipe-category">
+              {path === '/comidas/:id' ? recipe.strCategory : recipe.strAlcoholic}
+            </p>
+            <h2>Ingredients</h2>
+            {getIngredients(recipe, /strIngredient/).map((item, index) => {
+              const measure = getIngredients(recipe, /strMeasure/);
+              return (
+                <p
+                  key={ index }
+                  data-testid={ `${index}-ingredient-name-and-measure` }
+                >
+                  {`- ${item} - ${measure[index]} `}
+                </p>
+              );
+            })}
+            <h2>Intructions</h2>
+            <p data-testid="instructions">{ recipe.strInstructions }</p>
+            {recipe.strVideo === null || recipe.strYoutube === null
+              ? <p />
+              : (
+                <div>
+                  <h2>Video</h2>
+                  {
+                    !change
+                      ? <YouTube videoId={ getVideoId(recipe.strYoutube) } />
+                      : <iframe src={ recipe.strYoutube } data-testid="video" />
+                  }
+                  <button
+                    type="button"
+                    onClick={ () => (change ? setChange(false) : setChange(true)) }
+                  >
+                    Trocar fonte
+                  </button>
+                </div>
+              )}
+            <h2>Recomendadas</h2>
+            { !random ? <p>LOADING...</p>
+              : random.map((info, index) => (
+                <Cards
+                  key={ index }
+                  data-testid={ `${index}-recomendation-card` }
+                  recipe={ path === '/comidas/:id' ? 'bebidas' : 'comidas' }
+                  info={ info }
+                  index={ index }
+                />))}
+            { !random ? <p>LOADING...</p>
+              : random.map((info, index) => (
+                <Cards
+                  key={ index }
+                  data-testid={ `${index}-recomendation-card` }
+                  recipe={ path === '/comidas/:id' ? 'bebidas' : 'comidas' }
+                  info={ info }
+                  index={ index }
+                />))}
+            <button
+              type="button"
+              data-testid="start-recipe-btn"
+            >
+              Iniciar Receita
+            </button>
+          </div>))}
+    </div>
   );
 }
+
+RecipeDetails.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string,
+    params: PropTypes.shape({ id: PropTypes.string }),
+  }).isRequired,
+};
 
 export default RecipeDetails;
