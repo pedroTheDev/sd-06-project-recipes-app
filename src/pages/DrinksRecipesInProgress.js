d04341521833e5df7d427175e6cc436e042ef618
Tiago Berwanger <berwangertiago@gmail.com>
2020-11-25 17:24:28 -0300

    ajusting requirement 51

--- src/pages/DrinksRecipesInProgress.js
+++ src/pages/DrinksRecipesInProgress.js
@@ -63,7 +63,10 @@ class DrinksRecipesInProgress extends React.Component {
     const shareBtn = document.querySelector('.share-btn');
     const url = `http://localhost:3000/bebidas/${idDrink}/in-progress`;
     shareBtn.value = 'Link copiado!';
-    window.alert('Link copiado!');
+    const recipeButtons = document.querySelector('.recipe-buttons');
+    const span = document.createElement('span');
+    recipeButtons.appendChild(span);
+    span.innerHTML = 'Link copiado!';
     const el = document.createElement('textarea');
     el.value = url;
     el.setAttribute('readonly', '');
