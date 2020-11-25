528e7bd121bddbcb4f5955d2c7b7c31772d17196
Daniel Jesus <dev.danieljs@gmail.com>
2020-11-24 22:45:41 -0300

    style: stylizing buttons and centering content

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../style/Perfil.css';
 
 const Perfil = () => {
   const [storageEmail, setStorageEmail] = useState('');
@@ -20,37 +21,40 @@ const Perfil = () => {
   return (
     <section>
       <Header title="Perfil" />
-      <div>
-        <span data-testid="profile-email">{storageEmail}</span>
-      </div>
-      <div>
-        <Link to="/receitas-feitas">
-          <button
-            type="button"
-            data-testid="profile-done-btn"
-          >
-            Receitas Feitas
-          </button>
-        </Link>
-        <Link to="/receitas-favoritas">
-          <button
-            type="button"
-            data-testid="profile-favorite-btn"
-          >
-            Receitas Favoritas
-          </button>
+      <div className="container-profile">
+        <div className="email-profile">
+          <span data-testid="profile-email">{storageEmail}</span>
+        </div>
+        <div className="buttons-container">
+          <Link to="/receitas-feitas">
+            <button
+              type="button"
+              data-testid="profile-done-btn"
+            >
+              Receitas Feitas
+            </button>
+          </Link>
+          <Link to="/receitas-favoritas">
+            <button
+              type="button"
+              data-testid="profile-favorite-btn"
+            >
+              Receitas Favoritas
+            </button>
 
-        </Link>
-        <Link to="/">
-          <button
-            type="button"
-            onClick={ handleClick }
-            data-testid="profile-logout-btn"
-          >
-            Sair
-          </button>
+          </Link>
+          <Link to="/">
+            <button
+              type="button"
+              onClick={ handleClick }
+              data-testid="profile-logout-btn"
+            >
+              Sair
+            </button>
 
-        </Link>
+          </Link>
+        </div>
+
       </div>
       <Footer />
     </section>
