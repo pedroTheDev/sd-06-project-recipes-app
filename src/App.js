c217c4b13b3f3d5b0c5a5bde3283742ff42fecd1
Tiago Berwanger <berwangertiago@gmail.com>
2020-11-26 18:31:08 -0300

    remaping routes

--- src/App.js
+++ src/App.js
@@ -15,37 +15,23 @@ function App() {
     <div className="master-container">
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route exact path="/comidas/:id" component={ FoodsDetails } />
+        <Route path="/comidas/:id/in-progress" component={ FoodsRecipesInProgress } />
+        <Route path="/bebidas/:id/in-progress" component={ DrinksRecipesInProgress } />
         <Route
-          exact
-          path="/comidas/:id/in-progress"
-          component={ FoodsRecipesInProgress }
-        />
-        <Route path="/comidas" component={ Foods } />
-        <Route exact path="/bebidas/:id" component={ DrinksDetails } />
-        <Route
-          exact
-          path="/bebidas/:id/in-progress"
-          component={ DrinksRecipesInProgress }
-        />
-        <Route
-          exact
-          path="/explorar/comidas/area"
-          component={ ExploreFoodsByArea }
-        />
-        <Route
-          exact
           path="/explorar/comidas/ingredientes"
           component={ ExploreFoodsByIngredient }
         />
         <Route
-          exact
           path="/explorar/bebidas/ingredientes"
           component={ ExploreDrinksByIngredient }
         />
+        <Route path="/explorar/comidas/area" component={ ExploreFoodsByArea } />
         <Route path="/explorar/comidas" component={ ExploreFoods } />
         <Route path="/explorar/bebidas" component={ ExploreDrinks } />
+        <Route path="/comidas/:id" component={ FoodsDetails } />
+        <Route path="/bebidas/:id" component={ DrinksDetails } />
         <Route path="/bebidas" component={ Drink } />
+        <Route path="/comidas" component={ Foods } />
         <Route path="/explorar" component={ Explore } />
         <Route path="/perfil" component={ Profile } />
         <Route path="/receitas-feitas" component={ DoneRecipes } />
