3b7f1836fecc16c4f6c6c7aee5b6b31d1f1f93af
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-11-26 17:17:23 -0300

    Group 13 - Ajustes de CSS na tela login

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import RecipeList from '../components/RecipeList';
+import Footer from '../components/Footer';
 
 function Comidas() {
   return (
     <div>
       <Header />
       <RecipeList />
+      <Footer />
     </div>
   );
 }
