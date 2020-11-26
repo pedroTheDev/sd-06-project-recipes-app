6ea7640b8a5343e853b830f31192c83ffa691053
Victor Bergamini <vbergadev@gmail.com>
2020-11-26 10:34:31 -0300

    Tela Explorar

--- src/pages/ExplorarComidas.js
+++ src/pages/ExplorarComidas.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header2 from '../components/Header2';
 import MenuInferior from '../components/MenuInferior';
 
 function ExplorarComidas() {
+  const history = useHistory();
+
+  function redirectExplorarIngredientes() {
+    history.push('/explorar/comidas/ingredientes');
+  }
+  function redirectExplorarOrigem() {
+    history.push('/explorar/comidas/area');
+  }
   return (
     <div>
       <Header2 title="Explorar Comidas" />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ redirectExplorarIngredientes }
+      >
+        Por Ingredientes
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-by-area"
+        onClick={ redirectExplorarOrigem }
+      >
+        Por Local de Origem
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
       <MenuInferior />
     </div>
   );
