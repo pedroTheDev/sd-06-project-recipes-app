4b27ea117d88e921139e135d0de319ec57fac163
rodrigoderkian <derkianpromo@gmail.com>
2020-11-25 22:18:45 -0300

    Ingredients

--- src/pages/FoodDetails.js
+++ src/pages/FoodDetails.js
@@ -5,6 +5,7 @@ import { requestDetailsFood } from '../services/requestsAPI';
 function FoodDetails(props) {
   const { location: { state: { dataRecipes: { idMeal } } } } = props;
   const [foodDetails, setFoodDetails] = useState([]);
+  const [ingredients, setIngredients] = useState('');
 
   // async function handleRequisition() {
   //   const resultDetails = await requestDetailsFood(idMeal);
@@ -16,6 +17,13 @@ function FoodDetails(props) {
     async function fetchData() {
       const resultsDetails = await requestDetailsFood(idMeal);
       setFoodDetails(resultsDetails);
+      const meal = resultsDetails.meals[0];
+      const keysMeal = Object.keys(meal);
+      const filterMeal = keysMeal.filter((key) => key.toLowerCase().includes('ingredient'));
+      const filterMeasure = keysMeal.filter((key) => key.toLowerCase().includes('measure'));
+      const allIngredients = filterMeal
+        .map((item, index) => ({ ingredient: meal[item], measure: meal[filterMeasure[index]] }));
+      setIngredients(allIngredients);
     }
     fetchData();
     // const results = await handleRequisition();
@@ -32,10 +40,11 @@ function FoodDetails(props) {
         alt="Meal"
       />
       <h3 data-testid="recipe-title">{foodDetails.meals && foodDetails.meals[0].strMeal}</h3>
+      {ingredients && ingredients.map((item, index) => (<p>{`${index} ${item.ingredient} - ${item.measure}`}</p>))}
       <button type="button" data-testid="share-btn">Compartilhar</button>
       <button type="button" data-testid="favorite-btn">Favorito</button>
       <h4 data-testid="recipe-category">{foodDetails.meals && foodDetails.meals[0].strCategory}</h4>
-      {foodDetails.meals && foodDetails.meals.filter((ingredient) => ingredient.match(/strIngredient/))}
+
     </div>
   );
 }
