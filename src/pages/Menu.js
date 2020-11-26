3f38c04c230bf57be17656e744ea8fa92fb8dc85
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-11-26 18:45:14 -0300

    Ajustes arquivo Menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import RecipeList from '../components/RecipeList';
-import Footer from '../components/Footer';
 
 function Menu() {
   return (
