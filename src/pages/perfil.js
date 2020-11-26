6b9c44edb6098d39d35fe33ed343d1de53b80336
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2020-11-26 14:50:20 -0300

    render Profile Page

--- src/pages/perfil.js
+++ src/pages/perfil.js
@@ -3,10 +3,20 @@ import React, { Component } from 'react';
 import { Header, Footer } from '../components';
 
 class Perfil extends Component {
+  
   render() {
+    const emailLS = localStorage.getItem('user');
+    console.log(emailLS)
+
     return (
       <div>
         <Header title="Perfil" />
+        <div>
+          <h3 data-testid="profile-email">emailLS</h3>
+          <button data-testid="profile-done-btn">receitas feitas</button>
+          <button data-testid="profile-favorite-btn">receitas favoritas</button>
+          <button data-testid="profile-logout-btn">sair</button>
+        </div>
         <Footer title="Perfil" />
       </div>
     );
