614b86a290d28cdf5ea9b945c90b921884b38663
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-11-27 19:27:16 -0300

    button

--- src/pages/DoneRecipe.js
+++ src/pages/DoneRecipe.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 function DoneRecipe() {
-  const FALSE = false;
-  return (
-    <div>
-      <Header title="Receitas Feitas" search={ FALSE } />
-    </div>
-  );
+	const FALSE = false;
+	return (
+		<div>
+			<Header title="Receitas Feitas" search={FALSE} />
+			<Link to="/receitas-favoritas">
+				<button type="button" data-testid="profile-favorite-btn">
+					Receitas Favoritas
+        </button>
+			</Link>
+			<hr></hr>
+			<button
+				type="button"
+				data-testid="filter-by-all-btn"
+			>
+				Filter all
+        </button>
+			<hr></hr>
+			<button
+				type="button"
+				data-testid="filter-by-food-btn"
+			>
+				Filter food
+        </button>
+			<hr></hr>
+			<button
+				type="button"
+				data-testid="filter-by-drink-btn"
+			>
+				Filter Drink
+        </button>
+
+
+
+		</div>
+	);
 }
 
 export default DoneRecipe;
--- src/pages/FavRecipe.js
+++ src/pages/FavRecipe.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
 function FavRecipe() {
   const FALSE = false;
   return (
     <div>
       <Header title="Receitas Favoritas" search={ FALSE } />
+      <Link to="/receitas-feitas">
+        <button type="button" data-testid="profile-done-btn">
+          Receitas Feitas
+        </button>
+      </Link>
     </div>
   );
 }
