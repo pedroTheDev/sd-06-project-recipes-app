c53af61b429c2fd611718fdd81bd7c0c95743850
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-11-27 15:54:01 -0300

    fix eslint

--- src/Routes.js
+++ src/Routes.js
@@ -49,8 +49,14 @@ const Routes = () => {
         <Route exact path="/" component={ Login } />
         <Route path="/explorar/comidas/ingredientes" component={ IngredientFoods } />
         <Route path="/explorar/bebidas/ingredientes" component={ IngredientDrinks } />
-        <Route path="/comidas/:id/in-progress" render={ (props) => <FoodsDetails { ...props } /> } />
-        <Route path="/bebidas/:id/in-progress" render={ (props) => <DrinksDetails { ...props } /> } />
+        <Route
+          path="/comidas/:id/in-progress"
+          render={ (props) => <FoodsDetails { ...props } /> }
+        />
+        <Route
+          path="/bebidas/:id/in-progress"
+          render={ (props) => <DrinksDetails { ...props } /> }
+        />
         <Route path="/explorar/comidas/area" component={ OriginFoods } />
         <Route path="/explorar/comidas" component={ ExploreFoods } />
         <Route path="/explorar/bebidas" component={ ExploreDrinks } />
