eba3c0349ccca2fe74dda339268485a57968ae68
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-11-27 12:43:11 -0300

    Requirements done

--- src/components/ShareBtn.js
+++ src/components/ShareBtn.js
@@ -2,18 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import shareIcon from '../images/shareIcon.svg';
 
-function ShareBtn({ copy }) {
+function ShareBtn({ copy, testid, index }) {
   return (
     <button type="button" onClick={ copy }>
-      <img
-        data-testid="share-btn"
-        src={ shareIcon }
-        alt="shareButton"
-      />
+      { testid === 'done'
+        ? (
+          <img
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ shareIcon }
+            alt="shareButton"
+          />)
+        : (
+          <img
+            data-testid="share-btn"
+            src={ shareIcon }
+            alt="shareButton"
+          />)}
     </button>
   );
 }
 ShareBtn.propTypes = {
   copy: PropTypes.func.isRequired,
+  testid: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
 };
 export default ShareBtn;
