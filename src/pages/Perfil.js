5a11ef9ab22272965379d795ace884ca1a4a7b83
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-11-27 11:45:15 -0300

    Implementação da Tela de Perfil

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
@@ -8,11 +9,38 @@ import Header from '../components/Header';
 function Perfil(props) {
   const { history: { location: { pathname } }, pageConfig } = props;
   const { header } = pageConfig;
+  const [email, isLoading] = useState('');
 
   return (
     <div>
       <Header pathname={ pathname } componentConfig={ header } />
-      <p>página de perfil</p>
+      <section className="profile-container">
+        <section className="profile-email">
+          {!isLoading && <h3 data-testid="profile-email">{email}</h3>}
+        </section>
+        <section className="profile-buttons">
+          <Link to="/receitas-feitas">
+            <button data-testid="profile-done-btn" type="button">Receitas Feitas</button>
+          </Link>
+          <Link to="/receitas-favoritas">
+            <button
+              data-testid="profile-favorite-btn"
+              type="button"
+            >
+              Receitas Favoritas
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              data-testid="profile-logout-btn"
+              type="button"
+              onClick={ () => localStorage.clear() }
+            >
+              Sair
+            </button>
+          </Link>
+        </section>
+      </section>
       <Footer />
     </div>
   );
