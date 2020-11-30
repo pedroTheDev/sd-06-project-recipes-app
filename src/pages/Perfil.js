5b6b099bb4a8f61a9308a489482bb5e427b96c97
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-11-30 18:42:44 -0300

    Implementaçao inicial da tela de perfil

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Perfil() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    const retrievedUsername = JSON.parse(localStorage.getItem('user'));
+    setUser(retrievedUsername.email);
+    console.log(user);
+  }, []);
   return (
-    <main>
-      <Header pageName="Perfil" renderSearch={ false } />
-      Perfil Page
+    <main className="default-page">
+      <Header pageName="Perfil" renderSearch={false} />
+      <h2 data-testid="profile-email">{user}</h2>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+      >
+        Receitas Feitas</button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+      >
+        Receitas Favoritas</button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+      >
+        Sair
+      </button>
       <Footer />
     </main>
   );
