c41996e5c773bb31dbaf8a8b5311346ee5491df7
Alvaro Neto <alvaroshn@gmail.com>
2020-11-30 13:37:26 -0300

    tela de login test

--- src/tests/Tela_de_Login.test.js
+++ src/tests/Tela_de_Login.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-// import { fireEvent } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import { Login } from '../pages';
+import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 
 describe('Testar a página de login renderiza normalmente', () => {
@@ -47,5 +48,44 @@ describe('Testar a página de login renderiza normalmente', () => {
     inputPassword.value = '1234567';
 
     expect(submitBtn).toBeDisabled(false);
+  });
+});
+
+describe('Testar ações após o click do botão', () => {
+  it('Redireciona para a página de Comidas', () => {
+    const { getByTestId, history } = renderWithRouter(<App />);
+    history.push('/');
+
+    const inputEmail = getByTestId('email-input');
+    const inputPassword = getByTestId('password-input');
+    const submitBtn = getByTestId('login-submit-btn');
+
+    fireEvent.change(inputEmail, { target: { value: 'email@email.com' } });
+    fireEvent.change(inputPassword, { target: { value: '1234567' } });
+    fireEvent.click(submitBtn);
+
+    // expect(history.location.pathname).toBe('/comidas');
+    // não está redirecionando após o click
+  });
+
+  it('Cria as chaves no LocalStorage', () => {
+    const { getByTestId } = renderWithRouter(<Login />);
+
+    const inputEmail = getByTestId('email-input');
+    const inputPassword = getByTestId('password-input');
+    const submitBtn = getByTestId('login-submit-btn');
+
+    fireEvent.change(inputEmail, { target: { value: 'email@google.com' } });
+    fireEvent.change(inputPassword, { target: { value: '1234567' } });
+    fireEvent.click(submitBtn);
+
+    // const mealsToken = localStorage.getItem('mealsToken');
+    // const cocktailsToken = localStorage.getItem('cocktailsToken');
+    // const user = JSON.parse(localStorage.getItem('user'));
+
+    // expect(mealsToken).toBe('1');
+    // expect(cocktailsToken).toBe('1');
+    // expect(user.email).toBe(inputEmail.value);
+    // após o click não estão sendo criadas as chaves no LocalStorage
   });
 });
