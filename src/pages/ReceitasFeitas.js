23bef377980253d9a4be5260c282cbc0f0cdca51
LeandroParisi <lepcbelisario@gmail.com>
2020-11-30 15:59:58 -0300

    EsLint 2

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 
-function ReceitasFeitas(props) {
-  const { location } = props;
-  const { pathname } = location;
-  console.log('Path', pathname);
+function ReceitasFeitas() {
   return (
     <main>
       <Header pageName="Receitas Feitas" renderSearch={ false } />
