8076346c7f8ec1ebc0a4b955043078ed680e714b
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-11-30 17:32:31 -0300

    se n tiver favoritos

--- src/pages/FavRecipe.js
+++ src/pages/FavRecipe.js
@@ -15,6 +15,7 @@ function FavRecipe() {
   } = useContext(Context);
 
   const local = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  const ZERO = 0;
   const [filter, setFilter] = useState('all');
   const [update, setUpdate] = useState('no');
 
@@ -35,60 +36,66 @@ function FavRecipe() {
   return (
     <div>
       <Header title="Receitas Favoritas" />
-      <button
-        type="button"
-        onClick={ () => setFilter('all') }
-        data-testid="filter-by-all-btn"
-      >
-        All
-      </button>
-      <button
-        type="button"
-        onClick={ () => setFilter('comida') }
-        data-testid="filter-by-food-btn"
-      >
-        Food
-      </button>
-      <button
-        type="button"
-        onClick={ () => setFilter('bebida') }
-        data-testid="filter-by-drink-btn"
-      >
-        Drinks
-      </button>
-      {filters.map((recipe, index) => (
-        <div key={ recipe.id }>
-          <Link to={ `/${recipe.type}s/${recipe.id}` }>
-            <img
-              className="thumbnail"
-              data-testid={ `${index}-horizontal-image` }
-              src={ recipe.image }
-              alt={ `${recipe.type}_favorita` }
-            />
-          </Link>
-          <p data-testid={ `${index}-horizontal-top-text` }>
-            {recipe.area ? `${recipe.area} - ${recipe.category}` : recipe.alcoholicOrNot}
-          </p>
-          <Link to={ `/${recipe.type}s/${recipe.id}` }>
-            <h2 data-testid={ `${index}-horizontal-name` }>{ recipe.name }</h2>
-          </Link>
-          <input
-            type="image"
-            src={ shareIcon }
-            data-testid={ `${index}-horizontal-share-btn` }
-            alt="share-icon"
-            onClick={ () => share(`/${recipe.type}s/${recipe.id}`) }
-          />
-          { copied ? <span>Link copiado!</span> : '' }
-          <input
-            type="image"
-            src={ heart === 'white' ? whiteHeartIcon : blackHeartIcon }
-            data-testid={ `${index}-horizontal-favorite-btn` }
-            alt={ heart === 'white' ? 'whiteHeartIcon' : 'blackHeartIcon' }
-            onClick={ () => favorited(recipe) }
-          />
-        </div>
-      ))}
+      {!local || local.length === ZERO ? <h1>Sem receitas favoritas</h1>
+        : (
+          <div>
+            <button
+              type="button"
+              onClick={ () => setFilter('all') }
+              data-testid="filter-by-all-btn"
+            >
+              All
+            </button>
+            <button
+              type="button"
+              onClick={ () => setFilter('comida') }
+              data-testid="filter-by-food-btn"
+            >
+              Food
+            </button>
+            <button
+              type="button"
+              onClick={ () => setFilter('bebida') }
+              data-testid="filter-by-drink-btn"
+            >
+              Drinks
+            </button>
+            {filters.map((recipe, index) => (
+              <div key={ recipe.id }>
+                <Link to={ `/${recipe.type}s/${recipe.id}` }>
+                  <img
+                    className="thumbnail"
+                    data-testid={ `${index}-horizontal-image` }
+                    src={ recipe.image }
+                    alt={ `${recipe.type}_favorita` }
+                  />
+                </Link>
+                <p data-testid={ `${index}-horizontal-top-text` }>
+                  {recipe.area ? `${recipe.area} - ${recipe.category}`
+                    : recipe.alcoholicOrNot}
+                </p>
+                <Link to={ `/${recipe.type}s/${recipe.id}` }>
+                  <h2 data-testid={ `${index}-horizontal-name` }>{ recipe.name }</h2>
+                </Link>
+                <input
+                  type="image"
+                  src={ shareIcon }
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  alt="share-icon"
+                  onClick={ () => share(`/${recipe.type}s/${recipe.id}`) }
+                />
+                { copied ? <span>Link copiado!</span> : '' }
+                <input
+                  type="image"
+                  src={ heart === 'white' ? whiteHeartIcon : blackHeartIcon }
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                  alt={ heart === 'white' ? 'whiteHeartIcon' : 'blackHeartIcon' }
+                  onClick={ () => favorited(recipe) }
+                />
+              </div>
+            ))}
+          </div>
+        )}
     </div>
   );
 }
