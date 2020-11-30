50ca1bbb27513a60e719b4db888d0a86ac1374dd
Thales Rangel <thalesgr@gmail.com>
2020-11-30 15:12:22 -0300

    Clipboard na receitas feitas

--- src/pages/ReceitasFeitas.jsx
+++ src/pages/ReceitasFeitas.jsx
@@ -1,13 +1,23 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import copy from 'clipboard-copy';
 import Context from '../context/Context';
 import Header from '../Components/Header';
+import ShareIcon from '../images/shareIcon.svg';
 
 export default function ReceitasFeitas() {
   const { titulo, setTitulo } = useContext(Context);
+  const [sharedURL, setSharedURL] = useState(false);
 
   useEffect(() => {
     setTitulo('Receitas Feitas');
   }, []);
+
+  const urlToClipboard = () => {
+    const url = window.location.href;
+
+    copy(url);
+    setSharedURL(true);
+  };
 
   return (
     <div>
@@ -15,6 +25,17 @@ export default function ReceitasFeitas() {
       <button type="button" data-testid="filter-by-all-btn">All</button>
       <button type="button" data-testid="filter-by-food-btn">Food</button>
       <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+      <div>
+        <button
+          type="button"
+          alt="compartilhar"
+          data-testid="share-btn"
+          onClick={ urlToClipboard }
+        >
+          <img src={ ShareIcon } alt="compartilhar" />
+        </button>
+        {sharedURL ? <p>Link copiado!</p> : null}
+      </div>
     </div>
   );
 }
