308e7723992b2f11e5a46b863b96cc0415202f7c
DekoPossas <andrepossaster@gmail.com>
2020-12-01 15:55:17 -0300

    implementei o teste do 10.2

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -22,6 +22,41 @@ describe('9 - Implemente os elementos do header na tela principal de receitas, r
     expect(pageTitle).toBeInTheDocument();
     expect(SearchTopBar).toBeInTheDocument();
   })
+})
 
-  it('')
+describe('10 - Implemente um ícone para a tela de perfil, um título e um ícone para a busca, caso exista no protótipo', () => {
+
+  afterEach(cleanup);
+  
+  it('não deve ter os botões em Loguin', () => {
+    const { queryByTestId } = renderWithRouter(
+      <Provider>
+        <App />
+      </Provider>
+    );
+    
+    const profileTopBtn = queryByTestId('profile-top-btn')
+    const pageTitle = queryByTestId('page-title')
+    const SearchTopBar = queryByTestId('search-top-btn')
+
+    expect(profileTopBtn).toBe(null);
+    expect(pageTitle).toBe(null);
+    expect(SearchTopBar).toBe(null);
+  })
+
+  it('O header tem os ícones corretos na tela de principal de receitas de comidas', () => {
+    const { queryByTestId } = renderWithRouter(
+      <Provider>
+        <Comidas />
+      </Provider>
+    );
+
+    const profileTopBtn = queryByTestId('profile-top-btn')
+    const pageTitle = queryByTestId('page-title')
+    const SearchTopBar = queryByTestId('search-top-btn')
+
+    expect(profileTopBtn).toBeInTheDocument();
+    expect(pageTitle).toBeInTheDocument();
+    expect(SearchTopBar).toBeInTheDocument();
+  })
 })
