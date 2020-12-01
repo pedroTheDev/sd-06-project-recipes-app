2216ef462187a673d6497fafb6a2afdb1734cbd4
DekoPossas <andrepossaster@gmail.com>
2020-12-01 13:06:41 -0300

    criei a pasta para colocar os testes, alterei os teste para renderwithrouter

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { cleanup, fireEvent, render } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter'
-import Login from './Pages/Login'
 import App from './App'
 
 describe('2 - Crie todos os elementos que devem respeitar os atributos descritos no protótipo para a tela de login', () => {
   it('Tem os data-testids email-input, password-input e login-submit-btn', () => {
-    const { getByTestId } = render(<Login/>);
+    const { getByTestId } = renderWithRouter(<App/>);
     const emailInput = getByTestId('email-input');
     const passwordInput = getByTestId('password-input');
     const button = getByTestId('login-submit-btn');
@@ -19,7 +18,7 @@ describe('2 - Crie todos os elementos que devem respeitar os atributos descritos
 
 describe('3 - Desenvolva a tela de maneira que a pessoa deve conseguir escrever seu email no input de email', () => {
   it('É possível escrever o email', () => {
-    const { getByTestId } = render(<Login/>);
+    const { getByTestId } = renderWithRouter(<App/>);
     const emailInput = getByTestId('email-input');
 
     fireEvent.change(emailInput, { target: { value: 'email@email.com' } })
@@ -29,7 +28,7 @@ describe('3 - Desenvolva a tela de maneira que a pessoa deve conseguir escrever 
 
 describe('4 - Desenvolva a tela de maneira que a pessoa deve conseguir escrever sua senha no input de senha', () => {
   it('É possível escrever a senha', () => {
-    const { getByTestId } = render(<Login/>);
+    const { getByTestId } = renderWithRouter(<App/>);
     const passwordInput = getByTestId('password-input');
 
     fireEvent.change(passwordInput, { target: { value: '123456' } })
@@ -39,7 +38,7 @@ describe('4 - Desenvolva a tela de maneira que a pessoa deve conseguir escrever 
 
 describe('5 - Desenvolva a tela de maneira que o formulário só seja válido após um email válido e uma senha de mais de 6 caracteres serem preenchidos', () => {
   it('O botão deve estar desativado se o email for inválido', () => {
-    const { getByTestId } = render(<Login/>);
+    const { getByTestId } = renderWithRouter(<App/>);
     const emailInput = getByTestId('email-input');
     const passwordInput = getByTestId('password-input');
     const button = getByTestId('login-submit-btn');
@@ -53,7 +52,7 @@ describe('5 - Desenvolva a tela de maneira que o formulário só seja válido ap
   });
 
   it('O botão deve estar desativado se a senha deve tiver 6 caracteres ou menos', () => {
-    const { getByTestId } = render(<Login/>);
+    const { getByTestId } = renderWithRouter(<App/>);
     const emailInput = getByTestId('email-input');
     const passwordInput = getByTestId('password-input');
     const button = getByTestId('login-submit-btn');
@@ -65,7 +64,7 @@ describe('5 - Desenvolva a tela de maneira que o formulário só seja válido ap
   });
 
   it('O botão deve estar ativado se o email e a senha forem válidos', () => {
-    const { getByTestId } = render(<Login/>);
+    const { getByTestId } = renderWithRouter(<App/>);
     const emailInput = getByTestId('email-input');
     const passwordInput = getByTestId('password-input');
     const button = getByTestId('login-submit-btn');
