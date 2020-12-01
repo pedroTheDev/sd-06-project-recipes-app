d04b786b3b2e69a8bb4e1aa3984f96ec1b219f48
Marxneves <marxkhristie@gmail.com>
2020-12-01 15:44:06 -0300

    Tentando fazer os testes

--- src/_tests_/PrincipalReceitas.test.js
+++ src/_tests_/PrincipalReceitas.test.js
@@ -6,16 +6,24 @@ import App from '../App';
 // import Comidas from '../pages/Comidas';
 
 describe('Tela principal de receitas', () => {
-  it('A tela tem os data-testids de todos os 12 cards da tela de comidas', () => {
+  it('A tela tem os data-testids de todos os 12 cards da tela de comidas', async () => {
     const { getByText, history } = renderWithRouter(<App />);
     history.push('/comidas');
 
-    waitForElement(() => {
-      expect(getByText('Comidas')).toBeInTheDocument();
-    });
+    setTimeout(() => {
+      expect(getByText(/Comidas/i)).toBeInTheDocument();
+    }, 500)
+    
+    // await waitForElement(() => {
+    //   expect(getByText(/Comidas/i)).toBeInTheDocument();
+    // });
   });
   it('A tela tem os data-testids de todos os 12 cards da tela de bebidas', () => {
-    // const { getByText, history } = renderWithRouter(<App />);
-    // history.push('/bebidas');
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/bebidas');
+
+    setTimeout(() => {
+      expect(getByText(/Bebidas/i)).toBeInTheDocument();
+    }, 500)
   });
 });
