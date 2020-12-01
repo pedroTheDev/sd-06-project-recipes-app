0fcca2e53291f42f17732c23642500b3c787b198
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-12-01 16:16:42 -0300

    Estilos iniciais

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -50,7 +50,7 @@ const MainPage = (props) => {
     <div>
       <Header pageName={ pathname } renderSearch />
       <div className="default-page">
-        <CategoryMenu pathname={ pathname } />
+        <CategoryMenu className="sub-header" pathname={ pathname } />
         {
           checkRequestSize(recipeList)
         }
