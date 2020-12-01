34cd1d2673f1ca5cc05313686c4ff85a405bdc9a
Isadora Koga <isadorakoga710@gmail.com>
2020-12-01 18:15:37 -0300

    Update footer tests

--- src/Tests/renderWithRouter.js
+++ src/Tests/renderWithRouter.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import Provider from '../hooks/Provider';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
 function renderWithRouter(component) {
   const history = createMemoryHistory();
   return ({
-    ...render(<Router history={ history }>{ component }</Router>), history,
+    ...render(
+      <Provider>
+        <Router history={ history }>{ component }</Router>
+      </Provider>
+    ),
+    history, 
   });
 }
 
