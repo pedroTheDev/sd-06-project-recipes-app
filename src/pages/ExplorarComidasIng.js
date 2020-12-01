26ae7af6adc6387f71415562723207aeb32fd90e
Elisa <elisatcamillo@gmail.com>
2020-12-01 15:52:24 -0300

    passando req 77

--- src/pages/ExplorarComidasIng.js
+++ src/pages/ExplorarComidasIng.js
@@ -15,6 +15,7 @@ const ExplorarComidasIng = () => {
 
   useEffect(() => {
     listIngredients().then((response) => setIngredientList(response));
+    setMeals([{ teste: 'teste' }]);
   }, []);
   console.log(ingredientList);
   const zero = 0;
