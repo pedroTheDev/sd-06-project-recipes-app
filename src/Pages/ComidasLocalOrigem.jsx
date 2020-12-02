cb38343ca4c116f86e31f9d6f287f551c05c07b8
Jean Machado <jeanvictor.arq@gmail.com>
2020-12-02 20:14:04 -0300

    atualizando css

--- src/Pages/ComidasLocalOrigem.jsx
+++ src/Pages/ComidasLocalOrigem.jsx
@@ -9,6 +9,7 @@ import './ComidasLocalOrigem.css';
 const ComidasLocalOrigem = () => {
   const [stateNamesOrigins, setNamesOrigins] = useState();
   const [stateSugestionsFoods, setSugestionsFoods] = useState();
+  const [stateAllFoods, setAllFoods] = useState();
 
   const handleSearchExploreOrigin = async () => {
     const originsLocal = await ApiExploreByPlaceOfOrigin();
@@ -30,19 +31,69 @@ const ComidasLocalOrigem = () => {
     handleSugestedFoods();
   }, []);
 
+  const searchAllRecipesFoods = () => {
+    console.log('fvgdfgfd');
+    if (!stateAllFoods) {
+      return (
+        <div>Loading...</div>
+      );
+    }
+    return (
+      <div className="main-cards">
+        {stateSugestionsFoods.foods.meals.map((meal, index) => (
+          <div
+            className="card"
+            data-testid={ `${index}-recipe-card` }
+            key={ meal.strMeal }
+          >
+            <p data-testid={ `${index}-card-name` }>{meal.strMeal}</p>
+            <button
+              type="button"
+            >
+              <img
+                data-testid={ `${index}-card-img` }
+                src={ meal.strMealThumb }
+                alt={ meal.strMeal }
+              />
+            </button>
+          </div>
+        )) }
+      </div>
+    );
+  };
+
+  const searchAll = async (target) => {
+    if (target.value === 'All') {
+      const allFoods = await showSugestedFoods();
+
+      // console.log(allFoods);
+
+      setAllFoods({
+        ...stateAllFoods,
+        allFoods,
+      });
+
+      searchAllRecipesFoods();
+    } else {
+      return 'false';
+    }
+  };
+
   const number = 11;
 
   return (
     <div>
-      {console.log(stateSugestionsFoods)}
       <Header />
       <div className="container-select">
         <select
+          name="select"
           className="select"
           data-testid="explore-by-area-dropdown"
+          onChange={ ({ target }) => searchAll(target) }
         >
           <option>Escolha um local de origem</option>
           <option
+            value="All"
             data-testid="All-option"
           >
             All
@@ -51,6 +102,7 @@ const ComidasLocalOrigem = () => {
           && stateNamesOrigins.meals.map((origin, i) => (
             <option
               key={ i }
+              value={ origin.strArea }
               data-testid={ `${origin.strArea}-option` }
             >
               {origin.strArea}
@@ -58,6 +110,7 @@ const ComidasLocalOrigem = () => {
           ))}
         </select>
       </div>
+
       <div className="main-cards">
         {!stateSugestionsFoods ? <div>Loading...</div> : (
           stateSugestionsFoods.foods.meals.map((meal, index) => (
