8403f67a1dcf8c59210ba753d86907a31361557b
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-12-02 09:58:49 -0300

    pagina perfil

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -6,6 +6,7 @@ import '../style/Perfil.css';
 
 const Perfil = () => {
   const [storageEmail, setStorageEmail] = useState([]);
+  const [isFetching, setFetching] = useState(true);
 
   const localStorageEmail = localStorage.getItem('user');
 
@@ -15,57 +16,63 @@ const Perfil = () => {
       const emailObject = Object.values(emailJSON);
       setStorageEmail(emailObject);
     }
+
+    setFetching(false);
   }, [localStorageEmail]);
 
   const handleClick = () => {
     localStorage.clear();
   };
 
-  return (
-    <section>
-      <Header title="Perfil" />
-      <div className="col mt-5 h-100">
-        <div className="row justify-content-center py-3">
-          <span data-testid="profile-email">{storageEmail}</span>
-        </div>
-        <div className="row align-items-center h-50">
-          <div className="col">
-            <Link to="/receitas-feitas">
-              <button
-                type="button"
-                className="btn btn-lg btn-block"
-                style={ { background: '#6CDC3E' } }
-                data-testid="profile-done-btn"
-              >
-                Receitas Feitas
-              </button>
-            </Link>
-            <Link to="/receitas-favoritas">
-              <button
-                className="btn btn-lg btn-block"
-                type="button"
-                data-testid="profile-favorite-btn"
-                style={ { background: '#6CDC3E' } }
-              >
-                Receitas Favoritas
-              </button>
-            </Link>
-            <Link to="/">
-              <button
-                type="button"
-                onClick={ handleClick }
-                data-testid="profile-logout-btn"
-                className="btn btn-lg btn-block my-4"
-                style={ { background: '#6CDC3E' } }
-              >
-                Sair
-              </button>
-            </Link>
+  return ((isFetching)
+    ? <div>carregando...</div>
+    : (
+
+      <section>
+        <Header title="Perfil" />
+        <div className="col mt-5 h-100">
+          <div className="row justify-content-center py-3">
+            <span data-testid="profile-email">{storageEmail}</span>
+          </div>
+          <div className="row align-items-center h-50">
+            <div className="col">
+              <Link to="/receitas-feitas">
+                <button
+                  type="button"
+                  className="btn btn-lg btn-block"
+                  style={ { background: '#6CDC3E' } }
+                  data-testid="profile-done-btn"
+                >
+                  Receitas Feitas
+                </button>
+              </Link>
+              <Link to="/receitas-favoritas">
+                <button
+                  className="btn btn-lg btn-block"
+                  type="button"
+                  data-testid="profile-favorite-btn"
+                  style={ { background: '#6CDC3E' } }
+                >
+                  Receitas Favoritas
+                </button>
+              </Link>
+              <Link to="/">
+                <button
+                  type="button"
+                  onClick={ handleClick }
+                  data-testid="profile-logout-btn"
+                  className="btn btn-lg btn-block my-4"
+                  style={ { background: '#6CDC3E' } }
+                >
+                  Sair
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-      <Footer />
-    </section>
+        <Footer />
+      </section>
+    )
   );
 };
 
