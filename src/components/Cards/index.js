6b732874f3e5b10950af8621fc32fef4441ad8bf
Renan Cunha <renan2610@gmail.com>
2020-12-01 21:44:17 -0300

    retornando estado do requisito 36 para nao quebrar os outros req.

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -12,10 +12,11 @@ const Cards = () => {
 
   const showFoodResearch = () => {
     const number = 11;
+    // if (apiValueSearch.foods.meals && apiValueSearch.foods.meals.length === 1) {
+    //   const foodID = apiValueSearch.foods.meals[0].idMeal;
+    //   window.location.href = `http://localhost:3000/comidas/${foodID}`;
+    // } else {
     if (apiValueSearch.foods.meals && apiValueSearch.foods.meals.length === 1) {
-      const foodID = apiValueSearch.foods.meals[0].idMeal;
-      window.location.href = `http://localhost:3000/comidas/${foodID}`;
-    } else {
       return (
         apiValueSearch.foods.meals && apiValueSearch.foods.meals.map((meal, index) => {
           if (apiValueSearch.foods.meals === null) { console.log('null'); }
