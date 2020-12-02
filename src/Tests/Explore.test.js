ba7b2197288991f0e5f0336ccd7f69772ad3e523
Micaeli Teixeira <luime6@gmail.com>
2020-12-02 18:06:50 -0300

    :white_check_mark: Add tests
    Explorar

---
+++ src/Tests/Explore.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import Explorar from '../Pages/Explorar';
+
+describe('Crie os elementos que devem respeitar os atributos descritos no protótipo',
+  () => {
+    it('Tem os data-testids explore-food e explore-drinks',
+      () => {
+        const { getByTestId } = renderWithRouter(<Explorar />);
+        const FOOD = getByTestId('explore-food');
+        const DRINK = getByTestId('explore-drinks');
+
+        expect(FOOD).toBeInTheDocument();
+        expect(DRINK).toBeInTheDocument();
+      });
+
+    it('O nomes dos botões devem ser "Explorar Comidas" e "Explorar Bebidas"',
+      () => {
+        const { getByTestId, getByText } = renderWithRouter(<Explorar />);
+        const FOOD = getByTestId('explore-food');
+        const FOOD_NAME = getByText('Explorar Comidas');
+        const DRINK = getByTestId('explore-drinks');
+        const DRINK_NAME = getByText('Explorar Bebidas');
+
+        expect(FOOD).toBe(FOOD_NAME);
+        expect(DRINK).toBe(DRINK_NAME);
+      });
+
+    it('O nomes dos botões devem ser "Explorar Comidas" e "Explorar Bebidas"',
+      () => {
+        const { getByTestId } = renderWithRouter(<Explorar />);
+        const FOOD = getByTestId('explore-food');
+        const DRINK = getByTestId('explore-drinks');
+
+        expect(FOOD).toHaveAttribute('href', '/explorar/comidas');
+        expect(DRINK).toHaveAttribute('href', '/explorar/bebidas');
+      });
+  });
