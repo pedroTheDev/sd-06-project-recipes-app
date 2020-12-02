3c17d9d1f54ab8e7af2026efa89007d0a06411cb
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-12-02 10:16:59 -0300

    perfil

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -27,7 +27,6 @@ const Perfil = () => {
   return ((isFetching)
     ? <div>carregando...</div>
     : (
-
       <section>
         <Header title="Perfil" />
         <div className="col mt-5 h-100">
