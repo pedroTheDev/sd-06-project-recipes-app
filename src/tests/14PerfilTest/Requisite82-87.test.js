fb526dd384113367d4e5f0bf084c3b7e2daeccf7
Stefano <stefano.branz@gmail.com>
2020-12-03 13:46:37 -0300

    test implementation Ricipe Details and Perfil

--- src/tests/14PerfilTest/Requisite82-87.test.js
+++ src/tests/14PerfilTest/Requisite82-87.test.js
@@ -1,1 +1,29 @@
-test.skip('', () => {});
+import React from 'react';
+// import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter/renderWithRouter';
+import App from '../../App';
+
+beforeAll(() => {
+  const email = 'teste@teste.com';
+  localStorage.user = JSON.stringify({ email });
+});
+
+// beforeEach(() => {
+//   localStorage.clear();
+// });
+
+test.only('83 - Implemente a solução de maneira que o '
+  + 'e-mail da pessoa usuária deve estar visível', async () => {
+  const { findByTestId, history } = renderWithRouter(<App />);
+  history.push('perfil');
+
+  const profileEmail = await findByTestId('profile-email');
+  // console.log(profileEmail.innerHTML);
+  expect(profileEmail.innerHTML).toBe('teste@teste.com');
+
+  // fireEvent.click(favoriteWhiteHeart);
+
+  // const favoriteRecipes = localStorage.getItem('favoriteRecipes');
+  // // console.log(favoriteRecipes);
+  // expect(favoriteRecipes).toBe(
+});
