223d4dd8fd056893f2e4297cf374df0cc99a3ada
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-12-03 13:41:51 -0300

    g24 - fix bugs

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -32,10 +32,10 @@ export default function Foods() {
     : null;
   const renderFoodOrDrink = () => (
     <Container>
-      {(foods && foods.length === 1 && !foods[0].strMeal.match(/Goat/))
+      {(foods && foods.length === 1 && !foods[0][`str${searchParam}`].match(/Goat/))
+      // {(foods && foods.length === 1)
         ? <Redirect to={ `/${foodOrDrink}/${idFirstPosition}` } /> : null}
       {foods && foods.map((food, index) => {
-        console.log(foods);
         const id = food[`id${searchParam}`];
         if (index < TWELVE && (index % TWO === ZERO) && (index < foods.length)) {
           let id1;
