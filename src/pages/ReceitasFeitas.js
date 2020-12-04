95578b0e97fe170fa4e7df4959412a596c4999d4
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-12-04 13:26:55 -0300

    Inicio req 54

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -2,11 +2,60 @@ import React from 'react';
 import Header from '../components/Header';
 
 const ReceitasFeitas = () => {
-  return (
-    <section>
-      <Header title="Receitas Feitas" />
-      
-    </section>
+  const recipes = JSON.parse(localStorage.getItem('doneRecipes'));
+
+  console.log(recipes);
+  return ((!recipes)
+    ? <h1>No Recipes Yet!</h1>
+    : (
+      <section>
+        <Header title="Receitas Feitas" />
+        <div>
+          <button
+            type="button"
+            data-testid="filter-by-all-btn"
+          >
+            All
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-food-btn"
+          >
+            Food
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-drink-btn"
+          >
+            Drinks
+          </button>
+        </div>
+        {recipes.map((recipe, index) => (
+          <div>
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ recipe.image }
+              alt=""
+            />
+            <p
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              {recipe.category}
+            </p>
+            <h3
+              data-testid={ `${index}-horizontal-name` }
+            >
+              {recipe.name}
+            </h3>
+            <p
+              data-testid={ `${index}-horizontal-done-date` }
+            >
+              {recipe.doneDate}
+            </p>
+          </div>
+        ))}
+      </section>
+    )
   );
 }
 
