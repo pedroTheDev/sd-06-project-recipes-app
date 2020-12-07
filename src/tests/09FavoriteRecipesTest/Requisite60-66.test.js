5d7626b43c834bf49d7698a5ffb47ab7f8d12014
Stefano <stefano.branz@gmail.com>
2020-12-07 16:00:13 -0300

    test implementations 2

--- src/tests/09FavoriteRecipesTest/Requisite60-66.test.js
+++ src/tests/09FavoriteRecipesTest/Requisite60-66.test.js
@@ -1,1 +1,21 @@
-test.skip('', () => {});
+import React from 'react';
+// import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter/renderWithRouter';
+import App from '../../App';
+
+test('60 - Implemente os elementos da tela de '
+  + 'receitas favoritas (cumulativo com os atributos '
+  + 'em comum com a tela de receitas feitas) respeitando '
+  + 'os atributos descritos no protótipo', async () => {
+  const { findByTestId, history } = renderWithRouter(<App />);
+  history.push('receitas-favoritas');
+
+  const allFilterButton = await findByTestId('filter-by-all-btn');
+  const foodFilterButton = await findByTestId('filter-by-food-btn');
+  const drinksFilterButton = await findByTestId('filter-by-drink-btn');
+
+  expect(allFilterButton).toBeInTheDocument();
+  expect(foodFilterButton).toBeInTheDocument();
+  expect(drinksFilterButton).toBeInTheDocument();
+});
+
