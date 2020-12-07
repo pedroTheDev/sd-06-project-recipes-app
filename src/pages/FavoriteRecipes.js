6144d9b6a7c9c7e19f67d98adbedb552023d2c0a
fomes <contato.felipeluna@gmail.com>
2020-12-07 16:27:18 -0300

    favorite page fix

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import clipboardCopy from 'clipboard-copy';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 export default function FavoriteRecipes() {
-  const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  const favorites = JSON.parse(localStorage.getItem('favoriteRecipes')) || null;
   const alcoholic = favorites.map((e) => e.alcoholicOrNot);
   const [copied, setCopied] = useState(false);
 
@@ -27,70 +28,74 @@ export default function FavoriteRecipes() {
   }
 
   return (
-    <div>
+    <div className="food-container">
       <Header title="Receitas Favoritas" />
-      <div className="favorite-filters">
-        <button
-          type="button"
-          data-testid="filter-by-all-btn"
-        >
-          All
-        </button>
-        <button
-          type="button"
-          data-testid="filter-by-food-btn"
-        >
-          Food
-        </button>
-        <button
-          type="button"
-          data-testid="filter-by-drink-btn"
-        >
-          Drinks
-        </button>
-      </div>
-      {favorites.map((e, index) => (
-        <div key={ index } className="fav-card">
-          <Link to={ `/${e.type}s/${e.id}` }>
-            <p data-testid={ `${index}-horizontal-name` }>
-              { `${e.name}`}
-            </p>
 
-            <img
-              src={ e.image }
-              alt="foto"
-              className="fav-img"
-              data-testid={ `${index}-horizontal-image` }
-            />
-          </Link>
-
-          <p data-testid={ `${index}-horizontal-top-text` }>
-            { `${e.area} - ${e.category}` }
-            {alcoholic[index]}
-          </p>
-
+      <div className="body-perfil">
+        <div className="btn-sub-header">
           <button
             type="button"
-            data-testid={ `${index}-horizontal-share-btn` }
-            onClick={ () => handleShareClick(e.id) }
-            src={ shareIcon }
+            data-testid="filter-by-all-btn"
           >
-            <img src={ shareIcon } alt="share" />
-            Share
+            All
           </button>
-          {(copied) && <span>Link copiado!</span>}
-
           <button
             type="button"
-            onClick={ () => handleRemove(index) }
-            data-testid={ `${index}-horizontal-favorite-btn` }
-            src={ blackHeartIcon }
+            data-testid="filter-by-food-btn"
           >
-            <img src={ blackHeartIcon } alt="blackHeart" />
-            Remove Favorite
+            Food
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-drink-btn"
+          >
+            Drinks
           </button>
         </div>
-      ))}
+        {favorites.map((e, index) => (
+          <div key={ index } className="fav-card">
+            <Link to={ `/${e.type}s/${e.id}` }>
+              <p data-testid={ `${index}-horizontal-name` }>
+                { `${e.name}`}
+              </p>
+
+              <img
+                src={ e.image }
+                alt="foto"
+                className="fav-img"
+                data-testid={ `${index}-horizontal-image` }
+              />
+            </Link>
+
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              { `${e.area} - ${e.category}` }
+              {alcoholic[index]}
+            </p>
+
+            <button
+              type="button"
+              data-testid={ `${index}-horizontal-share-btn` }
+              onClick={ () => handleShareClick(e.id) }
+              src={ shareIcon }
+            >
+              <img src={ shareIcon } alt="share" />
+              Share
+            </button>
+            {(copied) && <span>Link copiado!</span>}
+
+            <button
+              type="button"
+              onClick={ () => handleRemove(index) }
+              data-testid={ `${index}-horizontal-favorite-btn` }
+              src={ blackHeartIcon }
+            >
+              <img src={ blackHeartIcon } alt="blackHeart" />
+              Remove Favorite
+            </button>
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 }
