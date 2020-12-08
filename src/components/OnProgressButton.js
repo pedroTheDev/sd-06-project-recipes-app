05d000cdb6bf6dd5ded9fa054a997a00089b2ce4
Pedro Eduardo <pedroedu77@gmail.com>
2020-12-08 13:55:55 -0300

    Todos os requsitos concluidos

---
+++ src/components/OnProgressButton.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function OnProgressButton() {
+
+function handleOnProgress() {
+    const [statusDone, setStatusDone] = useState([])
+    const [statusInProgress, setStatusInProgress] = useState([])
+}
+
+    return(
+        <button
+        onClick={}
+        >
+
+        </button>
+    );
+}
