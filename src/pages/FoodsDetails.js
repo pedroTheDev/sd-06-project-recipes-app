254a2d082ef89468062cd72a8758458649cd743c
Cyrano webdev <cyrano@cyranowebdev.com>
2020-12-08 17:05:30 -0300

    inserindo testid no video em FoodDetails.

--- src/pages/FoodsDetails.js
+++ src/pages/FoodsDetails.js
@@ -180,6 +180,7 @@ export default function FoodsDetails(props) {
             allow="encrypted-media"
             title="video"
             width="360"
+            data-testid="video"
           />
 
           {
