1a494ed689a597c58cdd5dee44d3a0daea8fde80
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-12-08 06:25:14 -0300

    [24]req5 fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ export default function Login() {
 
   const inputValidate = () => {
     const validEmail = (/\S+@\S+\.\S+/).test(email);
-    const validPassword = (password && (/.{5}/).test(password));
+    const validPassword = (password && (/.{6}/).test(password));
 
     if (validEmail && validPassword === true) {
       return setDisable(false);
